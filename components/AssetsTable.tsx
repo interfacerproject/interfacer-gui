fc5b322c205dff00a34d9142e6a7d7d172f4f9fb
phoebus-84 <ennio@dyne.org>
2022-09-14 01:36:13 +0200

    add profile page

--- components/AssetsTable.tsx
+++ components/AssetsTable.tsx
@@ -1,19 +1,129 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import Link from "next/link";
 import BrTable from "./brickroom/BrTable";
 import BrTags from "./brickroom/BrTags";
 import BrDisplayUser from "./brickroom/BrDisplayUser";
 import AssetImage from "./AssetImage";
+import {useTranslation} from "next-i18next";
+import {gql, useQuery} from "@apollo/client";
+import devLog from "../lib/devLog";
 
-const AssetsTable = ({assets, assetsHead}: { assets: Array<any>, assetsHead: Array<string> }) => {
+const AssetsTable = ({userid}:{userid?:string}) => {
+    const {t} = useTranslation('lastUpdatedProps')
+    const QUERY_ASSETS = gql`query ($first: Int, $after: ID, $last: Int, $before: ID) {
+  proposals(first: $first, after: $after, before: $before, last: $last) {
+    pageInfo {
+      startCursor
+      endCursor
+      hasPreviousPage
+      hasNextPage
+      totalCount
+      pageLimit
+    }
+    edges {
+      cursor
+      node {
+        id
+        name
+        primaryIntents {
+          action {
+            id
+          }
+          hasPointInTime
+          hasBeginning
+          hasEnd
+          resourceInventoriedAs {
+            conformsTo {
+              name
+            }
+            primaryAccountable{
+              name
+              id
+            }
+            name
+            id
+            note
+            onhandQuantity {
+              hasUnit {
+                label
+              }
+            }
+            images {
+              hash
+              name
+              mimeType
+            }
+          }
+        }
+        reciprocalIntents {
+          resourceQuantity {
+            hasNumericalValue
+            hasUnit {
+              label
+              symbol
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`
+    const queryResult = useQuery(QUERY_ASSETS, {variables: {last: 10}})
+    const updateQuery = (previousResult: any, {fetchMoreResult}: any) => {
+        if (!fetchMoreResult) {
+            return previousResult;
+        }
+
+        const previousEdges = previousResult.proposals.edges;
+        const fetchMoreEdges = fetchMoreResult.proposals.edges;
+
+        fetchMoreResult.proposals.edges = [...previousEdges, ...fetchMoreEdges];
+
+        return {...fetchMoreResult}
+    }
+    const getHasNextPage = queryResult.data?.proposals.pageInfo.hasNextPage;
+    const loadMore = () => {
+        if (queryResult.data && queryResult.fetchMore) {
+            const nextPage = getHasNextPage;
+            const before = queryResult.data.proposals.pageInfo.endCursor;
+
+            if (nextPage && before !== null) {
+                queryResult.fetchMore({updateQuery, variables: {before}});
+            }
+        }
+    }
+    // Poll interval that works with pagination
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            const total =
+                (queryResult.data?.proposals.edges.length || 0)
+
+            queryResult?.refetch({
+                ...queryResult.variables,
+                last: total
+            });
+        }, 7000);
+
+        return () => clearInterval(intervalId);
+    }, [
+        ...Object.values(queryResult.variables!).flat(),
+        queryResult.data?.proposals.pageInfo.startCursor
+    ]);
+    devLog(queryResult.data)
+    const assets = userid?
+        queryResult.data?.proposals.edges.filter((edge:any) => edge.node.primaryIntents[0].resourceInventoriedAs.primaryAccountable.id === userid) :
+        queryResult.data?.proposals.edges
+
 
     return (<>
-        <BrTable headArray={assetsHead}>
-            {assets.map((e) =>
+        <BrTable headArray={t('tableHead', {returnObjects: true})}>
+            {assets?.map((e: any) =>
                 <tr key={e.cursor}>
                     <td>
                         <div className="flex max-w-xs min-w-[10rem]">
-                           {e.node.primaryIntents[0].resourceInventoriedAs?.images[0] && <div className="w-2/5 flex-none">
+                            {e.node.primaryIntents[0].resourceInventoriedAs?.images[0] &&
+                            <div className="w-2/5 flex-none">
                                 <AssetImage
                                     image={{
                                         hash: e.node.primaryIntents[0].resourceInventoriedAs?.images[0]?.hash,
@@ -48,6 +158,9 @@ const AssetsTable = ({assets, assetsHead}: { assets: Array<any>, assetsHead: Arr
                 </tr>
             )}
         </BrTable>
+        <div className="grid grid-cols-1 gap-4 mt-4 place-items-center">
+            <button className="btn btn-primary" onClick={loadMore} disabled={!getHasNextPage}>{t('Load more')}</button>
+        </div>
     </>)
 }
 
--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -2,16 +2,21 @@ import type {NextPage} from 'next'
 import {useAuth} from "../../lib/auth";
 import {gql, useQuery} from "@apollo/client";
 import {useRouter} from "next/router";
-import Tabs from "../../components/Tabs";
-// import EventTable from "../../components/EventTable";
 import Spinner from "../../components/brickroom/Spinner";
 import ResourceTable from "../../components/ResourceTable";
 import devLog from "../../lib/devLog";
+import Avatar from "boring-avatars";
+import React from "react";
+import AssetsTable from "../../components/AssetsTable";
+import {serverSideTranslations} from "next-i18next/serverSideTranslations";
+import {GetStaticPaths} from "next";
+import {useTranslation} from "next-i18next";
 
 
 const Profile: NextPage = () => {
     const router = useRouter()
     const {id} = router.query
+    const {t} = useTranslation('ProfileProps')
     const FETCH_USER = gql(`query($id:ID!) {
   person(id:$id) {
     id
@@ -41,14 +46,67 @@ const Profile: NextPage = () => {
     return (<>
         {!user && <Spinner/>}
         {user && <>
-                <h2 className="mb-6">{user?.name}</h2>
-                <h3 className="mb-6">Name: {user?.name}</h3>
-                <h3 className="mb-6">UserName: {user?.user}</h3>
-                <h3 className="mb-6">Email: {user?.email}</h3>
-            {/*<Tabs tabsArray={tabsArray}/>*/}
+            <div className="grid grid-cols-1 md:grid-cols-2 md:pl-8 px-2 pt-8">
+                <div className="flex flex-col">
+                    <div className="flex flex-row">
+                        <h2 className="mb-6 pt-5 mr-2">
+                            {t("hi")}, <span className="text-primary">{user?.name}</span>
+                        </h2>
+                        <div className="w-10 rounded-full">
+                            <Avatar
+                                size={'full'}
+                                name={authUsername}
+                                variant="beam"
+                                colors={["#F1BD4D", "#D8A946", "#02604B", "#F3F3F3", "#014837"]}
+                            />
+                        </div>
+                    </div>
+                    <p>{t('description')} </p>
+                    <h4 className="mt-2">Lorem ipsum <span className="text-primary">Dolor sit amet</span></h4>
+                </div>
+                <div className="flex flex-col">
+                    <div className="border-2 rounded h-60 w-full md:w-4/5 mx-auto p-4">
+                        <h2>{t('your stats')}:</h2>
+                        <p className="my-8">{t('statText')}</p>
+                        <div className="grid grid-cols-2">
+                            <div>
+                                <span className="text-4xl">&#127919; 120</span>
+                                <h3 className="text-slate-300">{t('goals')}</h3>
+                            </div>
+                            <div>
+                                <span className="text-4xl">&#x1F4AA; 84</span>
+                                <h3 className="text-slate-300">{t('strength')}</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id="tabs" className="my-6 space-x-4">
+                    <button className="px-12 text-black bg-gray-300 border-0 rounded-lg btn">{t('activity by the user')}
+                    </button>
+                    <span className="rounded-lg btn btn-disabled invisible lg:visible">Saved list</span>
+                </div>
+            </div>
+            <div className="grid grid-cols-1 md:px-8 px-2 pt">
+                <AssetsTable userid={idToBeFetch}/>
+            </div>
         </>}
     </>)
 };
+export const getStaticPaths: GetStaticPaths<{ slug: string }> = async () => {
+
+    return {
+        paths: [], //indicates that no page needs be created at build time
+        fallback: 'blocking' //indicates the type of fallback
+    }
+}
+
+export async function getStaticProps({locale}: any) {
+    return {
+        props: {
+            ...(await serverSideTranslations(locale, ['signInProps', 'lastUpdatedProps', 'SideBarProps', 'ProfileProps'])),
+        },
+    };
+}
 
 export default Profile
 
