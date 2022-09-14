2cbbddd4a1f5ee0bb23ca5dbc6bdb410d7a2b653
Puria Nafisi Azizi <puria@dyne.org>
2022-09-14 08:44:36 +0200

    test: Add french to test translations

--- components/LocationMenu.tsx
+++ components/LocationMenu.tsx
@@ -1,16 +1,21 @@
 import BrSelect from "./brickroom/BrSelect";
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 
-const LocationMenu = ({className}: {className?: string}) => {
+const LocationMenu = ({ className }: { className?: string }) => {
     const router = useRouter()
-    const {pathname, asPath, query, locale} = router
+    const { pathname, asPath, query, locale } = router
     const handleSelect = (e: any) => {
         e.preventDefault()
-        router.push({pathname, query}, asPath, {locale: e.target.value})
+        router.push({ pathname, query }, asPath, { locale: e.target.value })
     }
     return <BrSelect className={className}
-                     array={[{id: 'en', name: 'en'}, {id: 'de', name: 'de'}, {id: 'it', name: 'it'}]}
-                     handleSelect={handleSelect}
-                     value={locale} roundedLG/>
+        array={[
+            { id: 'en', name: 'en' },
+            { id: 'de', name: 'de' },
+            { id: 'fr', name: 'fr' },
+            { id: 'it', name: 'it' }
+        ]}
+        handleSelect={handleSelect}
+        value={locale} roundedLG />
 }
 export default LocationMenu
