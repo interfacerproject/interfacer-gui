899ed312940a3baf24399786ab5e438358d9b40a
phoebus-84 <ennio@dyne.org>
2022-09-14 18:41:53 +0200

    new sign in layout

--- pages/sign_in.tsx
+++ pages/sign_in.tsx
@@ -1,7 +1,6 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, ReactElement, useState} from 'react';
 import {useAuth} from "../lib/auth";
 import {useRouter} from "next/router";
-import Card, {CardWidth} from "../components/brickroom/Card";
 import BrInput from "../components/brickroom/BrInput";
 import Link from "next/link";
 import KeyringGeneration from "../components/KeyringGeneration";
@@ -9,6 +8,8 @@ import VerifySeed from "../components/VerifySeed";
 import devLog from "../lib/devLog";
 import {useTranslation} from "next-i18next";
 import {serverSideTranslations} from "next-i18next/serverSideTranslations";
+import Layout from "../components/SignInLayout";
+import {NextPageWithLayout} from "./_app";
 
 
 export async function getStaticProps({locale}: any) {
@@ -19,7 +20,8 @@ export async function getStaticProps({locale}: any) {
     };
 }
 
-export default function Sign_in() {
+
+const Sign_in: NextPageWithLayout = () => {
     const [isPassprhase, setIsPassphrase] = useState(false)
     const [step, setStep] = useState(0)
     const [email, setEmail] = useState('')
@@ -60,51 +62,58 @@ export default function Sign_in() {
     }
 
     return (
-        <div className="h-screen bg-cover"
-             style={{['backgroundImage' as any]: "url('https://www.interfacerproject.eu/assets/index/ABOUT.png')"}}>
-            <div className="container mx-auto h-screen grid place-items-center">
-                <Card title={t('title')}
-                      width={CardWidth.LG}
-                      className="md:px-16 py-[4.5rem]">
-                    <>
-                        {step === 0 && <><p>{t('presentation')}</p>
-                            <div className="w-full border-y-2 h-2 my-2"/>
-                            <Link href={'/sign_up'}>
-                                <a className="btn btn-block">{t('button3')}</a>
-                            </Link>
-                                <div className="w-full border-y-2 h-2 my-2"/>
-                            <button className="btn btn-block" type="button"
-                                    onClick={() => viaPassphrase()}>{t('button1')}</button>
-                            <button className="btn btn-block my-4" type="button"
-                                    onClick={() => viaQuestions()}>{t('button2')}</button>
-                        </>}
+        <div className="h-full grid grid-cols-6 md:mt-40 mt-2">
+            <div className="col-span-6 p-2 md:col-span-4 md:col-start-2 md:col-end-6">
+                <h2>{t('title')}</h2>
+                <div className="w-full border-y-2 h-2 my-2"/>
+                {step === 0 && <><p>{t('presentation')}</p>
+                    <div className="w-full border-y-2 h-2 my-2"/>
 
+                    <div className="w-full border-y-2 h-2 my-2"/>
+                    <button className="btn btn-block btn-primary" type="button"
+                            onClick={() => viaPassphrase()}>{t('button1')}</button>
+                    <button className="btn btn-block my-4 btn-primary" type="button"
+                            onClick={() => viaQuestions()}>{t('button2')}</button>
+                    <p className="flex flex-row items-center justify-between">
+                        {t('register.question')}
+                        <Link href={'/sign_up'}>
+                            <a>{t('register.answer')}</a>
+                        </Link>
 
-                        {step === 1 && <>
-                            <BrInput type="email" label={t('email.label')}
-                                     error={errorMail}
-                                     placeholder={t('email.placeholder')}
-                                     onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}/>
-                            {!isPassprhase && <>
-                                <button className="btn btn-block" type="button" onClick={() => toNextStep(2)}>
-                                    {t('button4')}
-                                </button>
-                            </>}
-                            {isPassprhase && <>
-                                <button className="btn btn-block" type="button" onClick={() => toNextStep(3)}>
-                                    {t('button4')}
-                                </button>
-                            </>}
-                        </>}
-                        {step === 3 && <>
-                            <VerifySeed email={email} HMAC={pdfk}/>
-                        </>}
-                        {step === 2 && <>
-                            <KeyringGeneration email={email} HMAC={pdfk}/>
-                        </>}
-                    </>
-                </Card>
+                    </p>
+                </>}
+                {step === 1 && <>
+                    <BrInput type="email" label={t('email.label')}
+                             error={errorMail}
+                             className={"w-full"}
+                             placeholder={t('email.placeholder')}
+                             onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}/>
+                    {!isPassprhase && <>
+                        <button className="btn btn-block" type="button" onClick={() => toNextStep(2)}>
+                            {t('button4')}
+                        </button>
+                    </>}
+                    {isPassprhase && <>
+                        <button className="btn btn-block" type="button" onClick={() => toNextStep(3)}>
+                            {t('button4')}
+                        </button>
+                    </>}
+                </>}
+                {step === 3 && <>
+                    <VerifySeed email={email} HMAC={pdfk}/>
+                </>}
+                {step === 2 && <>
+                    <KeyringGeneration email={email} HMAC={pdfk}/>
+                </>}
             </div>
         </div>
     )
 }
+Sign_in.getLayout = function getLayout(page: ReactElement) {
+    return (
+        <Layout>
+            {page}
+        </Layout>
+    )
+}
+export default Sign_in
