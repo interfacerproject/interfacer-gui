be1957b490a7eef886a69ec2d229e89bab8dcbe8
phoebus-84 <83974413+phoebus-84@users.noreply.github.com>
2022-09-14 16:01:19 +0200

    feat: add profile page (#53)
    * add profile page
    
    * in profile page use refetch to fill results from one user
    
    Co-authored-by: phoebus-84 <ennio@dyne.org>
    Co-authored-by: Puria Nafisi Azizi <puria@dyne.org>

--- components/AssetsTable.tsx
+++ components/AssetsTable.tsx
@@ -1,18 +1,130 @@
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import BrTable from "./brickroom/BrTable";
 import BrTags from "./brickroom/BrTags";
 import BrDisplayUser from "./brickroom/BrDisplayUser";
 import AssetImage from "./AssetImage";
+import { useTranslation } from "next-i18next";
+import { gql, useQuery } from "@apollo/client";
+import devLog from "../lib/devLog";
 
-const AssetsTable = ({assets, assetsHead}: { assets: Array<any>, assetsHead: Array<string> }) => {
+const AssetsTable = ({ userid }: { userid?: string }) => {
+    const { t } = useTranslation('lastUpdatedProps')
+    const QUERY_ASSETS = gql`query ($first: Int, $after: ID, $last: Int, $before: ID) {
+  proposals(first: $first, after: $after, before: $before, last: $last) {
+    pageInfo {
+      startCursor
+      endCursor
+      hasPreviousPage
+      hasNextPage
+      totalCount
+      pageLimit
+    }
+    edges {
+      cursor
+      node {
+        id
+        name
+        created
+        primaryIntents {
+          action {
+            id
+          }
+          hasPointInTime
+          hasBeginning
+          hasEnd
+          resourceInventoriedAs {
+            conformsTo {
+              name
+            }
+            primaryAccountable{
+              name
+              id
+            }
+            name
+            id
+            note
+            onhandQuantity {
+              hasUnit {
+                label
+              }
+            }
+            images {
+              hash
+              name
+              mimeType
+              bin
+            }
+          }
+        }
+        reciprocalIntents {
+          resourceQuantity {
+            hasNumericalValue
+            hasUnit {
+              label
+              symbol
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`
+    const queryResult = useQuery(QUERY_ASSETS, { variables: { last: 10 } })
+    const updateQuery = (previousResult: any, { fetchMoreResult }: any) => {
+        if (!fetchMoreResult) {
+            return previousResult;
+        }
+
+        const previousEdges = previousResult.proposals.edges;
+        const fetchMoreEdges = fetchMoreResult.proposals.edges;
+
+        fetchMoreResult.proposals.edges = [...previousEdges, ...fetchMoreEdges];
+
+        return { ...fetchMoreResult }
+    }
+    const getHasNextPage = queryResult.data?.proposals.pageInfo.hasNextPage;
+    const loadMore = () => {
+        if (queryResult.data && queryResult.fetchMore) {
+            const nextPage = getHasNextPage;
+            const before = queryResult.data.proposals.pageInfo.endCursor;
+
+            if (nextPage && before !== null) {
+                queryResult.fetchMore({ updateQuery, variables: { before } });
+            }
+        }
+    }
+    const assets = userid ?
+        queryResult.data?.proposals.edges.filter((edge: any) => edge.node.primaryIntents[0].resourceInventoriedAs.primaryAccountable.id === userid) :
+        queryResult.data?.proposals.edges
+    // Poll interval that works with pagination
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            const total =
+                ((queryResult.data?.proposals.edges.length * 2) - assets.length || 0)
+
+            queryResult?.refetch({
+                ...queryResult.variables,
+                last: total
+            });
+        }, 10000);
+
+        return () => clearInterval(intervalId);
+    }, [
+        ...Object.values(queryResult.variables!).flat(),
+        queryResult.data?.proposals.pageInfo.startCursor
+    ]);
+    devLog(queryResult.data)
 
     return (<>
-        <BrTable headArray={assetsHead}>
-            {assets?.map((e) =><>
+        <BrTable headArray={t('tableHead', {returnObjects: true})}>
+            {assets?.map((e: any) => <>
                 {e.node.primaryIntents.length > 0 && <tr key={e.cursor}>
                     <td>
                         <div className="grid grid-col-1 mx-auto md:mx-0 md:flex max-w-xs min-w-[10rem]">
-                           {e.node.primaryIntents[0].resourceInventoriedAs?.images[0] && <div className="flex-none w-full md:w-2/5">
+                           {e.node.primaryIntents[0].resourceInventoriedAs?.images[0] &&
+                              <div className="flex-none w-full md:w-2/5">
                                 <AssetImage
                                     image={e.node.primaryIntents[0].resourceInventoriedAs?.images[0]}
                                     className="mr-1 max-h-20"/>
@@ -27,7 +139,7 @@ const AssetsTable = ({assets, assetsHead}: { assets: Array<any>, assetsHead: Arr
                         </div>
                     </td>
                     <td className="">
-                        {new Date(e.node.created).toISOString()}
+                        {e.node?.created && new Date(e.node.created).toLocaleString()}
                     </td>
                     <td>
                         <h3>{e.node.reciprocalIntents[0].resourceQuantity.hasNumericalValue}</h3>
@@ -35,14 +147,18 @@ const AssetsTable = ({assets, assetsHead}: { assets: Array<any>, assetsHead: Arr
                     </td>
                     <td>
                         <BrDisplayUser id={e.node.primaryIntents[0].resourceInventoriedAs.primaryAccountable.id}
-                                       name={e.node.primaryIntents[0].resourceInventoriedAs.primaryAccountable.name}/>
+                            name={e.node.primaryIntents[0].resourceInventoriedAs.primaryAccountable.name} />
                     </td>
                     <td className="max-w-[12rem]">
-                        <BrTags tags={['this', 'tags', 'are', 'fakes']}/>
+                        <BrTags tags={[]} />
                     </td>
-                </tr>}</>
+                </tr>
+            }</>
             )}
         </BrTable>
+        <div className="grid grid-cols-1 gap-4 mt-4 place-items-center">
+            <button className="btn btn-primary" onClick={loadMore} disabled={!getHasNextPage}>{t('Load more')}</button>
+        </div>
     </>)
 }
 
--- components/LoginMenu.tsx
+++ components/LoginMenu.tsx
@@ -1,33 +1,37 @@
 import Link from "next/link";
-import {useAuth} from "../lib/auth";
+import { useAuth } from "../lib/auth";
 import React from "react";
 import Avatar from "boring-avatars";
 
 export default function LoginBtn() {
-    const {signOut, authUsername} = useAuth()
+    const { signOut, authUsername } = useAuth()
 
     return (
         <>
             <div className="mt-1 btn btn-ghost btn-block text-primary w-60 hover:bg-transparent">
-            <span className="w-full text-left flex flex-row items-center pl-3">
-                <div className="pl-0 grid grid-cols-2 p-2 items-center">
-                     <label tabIndex={0} className="btn btn-ghost btn-circle avatar bordered border-accent">
-                        <div className="w-10 rounded-full">
-                            <Avatar
-                                size={'full'}
-                                name={authUsername}
-                                variant="beam"
-                                colors={["#F1BD4D", "#D8A946", "#02604B", "#F3F3F3", "#014837"]}
-                            />;
+                <span className="flex flex-row items-center w-full pl-3 text-left">
+                    <div className="grid items-center grid-cols-2 p-2 pl-0">
+                        <label tabIndex={0} className="btn btn-ghost btn-circle avatar bordered border-accent">
+                            <div className="w-10 rounded-full">
+                                <Link href="/profile/my_profile">
+                                    <a>
+                                        <Avatar
+                                            size={'full'}
+                                            name={authUsername}
+                                            variant="beam"
+                                            colors={["#F1BD4D", "#D8A946", "#02604B", "#F3F3F3", "#014837"]}
+                                        />;
+                                    </a>
+                                </Link>
+                            </div>
+                        </label>
+                        <div className="grid grid-cols-1 ml-1 text-xs font-normal normal-case gap-y-1">
+                            <Link href="/profile/my_profile"><a><p className="text-base-400 whitespace-nowrap test-2xs">{authUsername}</p></a></Link>
+                            <button className="text-left hover:text-accent" onClick={() => signOut()}>Sign Out</button>
                         </div>
-                    </label>
-                    <div className="ml-1 grid grid-cols-1 text-xs font-normal normal-case gap-y-1">
-                        <Link href="/profile/my_profile"><a><p className="text-base-400 whitespace-nowrap test-2xs">{authUsername}</p></a></Link>
-                        <button className="hover:text-accent text-left" onClick={() => signOut()}>Sign Out</button>
-                    </div>
 
-                </div>
-            </span>
+                    </div>
+                </span>
 
             </div>
         </>
