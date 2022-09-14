c8f4103b54da85072be92550962c93398aafec9e
phoebus-84 <ennio@dyne.org>
2022-09-14 19:12:24 +0200

    fix auth tests

--- components/SignInLayout.tsx
+++ components/SignInLayout.tsx
@@ -14,22 +14,17 @@ const Layout: React.FunctionComponent<layoutProps> = (layoutProps: layoutProps) 
 
     return (
         <>
-            <div className="drawer drawer-mobile">
-                <input id="my-drawer" type="checkbox" className="drawer-toggle"/>
-                <div className="drawer-content">
-                    <Topbar search={false} userMenu={false} cta={layoutProps.cta}>
-                        <div className="w-auto h-16 p-4 border-r mx-2 hidden md:block">
-                            <div className="logo mx-auto"/>
-                        </div>
-                    </Topbar>
-                    <div className="container bg-[#F3F3F1] min-h-screen max-w-full">
-                        <div className="grid grid-cols-1 md:grid-cols-2">
-                            <img src="https://www.interfacerproject.eu/assets/index/ABOUT.png"
-                                 className={'w-full h-full object-cover md:h-screen hidden md:block'}/>
-                            <div className="h-full">
-                                {layoutProps?.children}
-                            </div>
-                        </div>
+            <Topbar search={false} userMenu={false} cta={layoutProps.cta}>
+                <div className="w-auto h-16 p-4 border-r mx-2 hidden md:block">
+                    <div className="logo mx-auto"/>
+                </div>
+            </Topbar>
+            <div className="container bg-[#F3F3F1] min-h-screen max-w-full">
+                <div className="grid grid-cols-1 md:grid-cols-2">
+                    <img src="https://www.interfacerproject.eu/assets/index/ABOUT.png"
+                         className={'w-full h-full object-cover md:h-screen hidden md:block'}/>
+                    <div className="h-full">
+                        {layoutProps?.children}
                     </div>
                 </div>
             </div>
