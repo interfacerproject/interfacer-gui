44d5c513aed371cfd269f1f81352a76717422010
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-09-28 12:02:37 +0200

    added e2e functionality test

---
+++ cypress/e2e/components/BrDisplayUser.cy.tsx
@@ -0,0 +1,14 @@
+describe("BrDisplayUser component", () => {
+    it("should click the component and check if navigation works", () => {
+        cy.visit("/");
+        // // Mounting component
+        // const name = "Mino";
+        // const location = "Bari";
+        // cy.mount(<BrDisplayUser id="1" name={name} location={location} />);
+
+        // // Visibility check
+        // cy.contains(name).should("be.visible");
+        // cy.contains(location).should("be.visible");
+        // cy.get("svg").should("be.visible");
+    });
+});
