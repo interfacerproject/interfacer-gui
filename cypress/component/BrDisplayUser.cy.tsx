3d2cae7c245659701f39a859f7d4c6ed9db7b58d
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-09-28 12:02:26 +0200

    fixed test description

--- cypress/component/BrDisplayUser.cy.tsx
+++ cypress/component/BrDisplayUser.cy.tsx
@@ -2,23 +2,19 @@ import React = require("react");
 import BrDisplayUser from "../../components/brickroom/BrDisplayUser";
 import "../../styles/globals.scss";
 
-describe("AddContributors.cy.tsx", () => {
-    it("something", () => {
-        /**
-         * Setup
-         */
-
+describe("BrDisplayUser component", () => {
+    it("should mount and check that all the info are visible", () => {
         // Mounting component
         const name = "Mino";
         const location = "Bari";
         cy.mount(<BrDisplayUser id="1" name={name} location={location} />);
 
         // Visibility check
+        cy.get("a").should("be.visible");
         cy.contains(name).should("be.visible");
         cy.contains(location).should("be.visible");
         cy.get("svg").should("be.visible");
-
-        // Click on link does not work because <link> works inside the app
-        // Not in component testing
     });
+
+    // Functionality is tested here:
 });
