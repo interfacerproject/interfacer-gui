84670c54db3210363eb0ca3b8aa5d79daddec2b5
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-09-28 12:14:28 +0200

    bugfix: sometimes primaryIntents[0] does not exist

--- components/AssetsTable.tsx
+++ components/AssetsTable.tsx
@@ -104,7 +104,7 @@ const AssetsTable = ({ userid }: { userid?: string }) => {
     const assets = userid
         ? queryResult.data?.proposals.edges.filter(
               (edge: any) =>
-                  edge.node.primaryIntents[0].resourceInventoriedAs
+                  edge.node.primaryIntents[0]?.resourceInventoriedAs
                       .primaryAccountable.id === userid
           )
         : queryResult.data?.proposals.edges;
