a3700439d3ae6801681fdd28a0600600af0ea689
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-09-28 10:32:24 +0200

    created TagSelector test

---
+++ cypress/component/TagSelector.cy.tsx
@@ -0,0 +1,56 @@
+import React = require("react");
+import TagSelector from "../../components/brickroom/TagSelector";
+import "../../styles/globals.scss";
+
+describe("AddContributors.cy.tsx", () => {
+    it("something", () => {
+        /**
+         * Setup
+         */
+
+        // Mounting component
+        cy.mount(<TagSelector></TagSelector>);
+
+        // Getting textarea
+        const input = cy.get("textarea");
+
+        // Function to select badges
+        const getBadgeByText = (text: string) => {
+            return cy.contains(text).filter(".badge");
+        };
+
+        /**
+         * Checking if badge gets created
+         */
+
+        // Variables
+        const word1 = "test";
+        // Typing in input
+        input.type(word1);
+        // Getting badge
+        const badge1 = getBadgeByText(word1);
+        // Exists?
+        badge1.should("exist");
+        // Has text?
+        badge1.should("include.text", word1);
+
+        /**
+         * Checking if other badges get created
+         */
+
+        for (let s of ["mario", "naso"]) {
+            input.type(" ");
+            input.type(s);
+            getBadgeByText(s).should("exist");
+        }
+
+        /**
+         * Deleting first badge
+         */
+
+        // Clicking the button
+        badge1.children("button").click();
+        // Checking that badge isn't visible anymore
+        badge1.should("not.exist");
+    });
+});
