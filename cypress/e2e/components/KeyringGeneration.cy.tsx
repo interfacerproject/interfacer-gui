c215b62fac4fd1d74a109709225c5f285f2998ef
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-10-03 15:49:10 +0200

    KeyringGeneration added test

---
+++ cypress/e2e/components/KeyringGeneration.cy.tsx
@@ -0,0 +1,36 @@
+describe("BrDisplayUser component", () => {
+    // before(() => {
+    //     cy.login();
+    //     cy.saveLocalStorage();
+    // });
+
+    // beforeEach(() => {
+    //     cy.visit("/assets");
+    //     cy.restoreLocalStorage();
+    // });
+
+    it("should go to /sign_up page", () => {
+        cy.visit("/sign_up");
+    });
+
+    it("should go to /sign_in page", () => {
+        cy.visit("/sign_in");
+    });
+
+    // it("should click the component and go to user page", () => {
+    //     // Waiting for data
+    //     const name = "dataGetFirst";
+    //     cy.intercept("http://65.109.11.42:9000/api").as(name);
+    //     cy.wait(`@${name}`);
+
+    //     // Getting the first instance of the component
+    //     const comp = cy.get("tr > td > a").first();
+    //     // Checking if it's visible
+    //     comp.should("be.visible");
+    //     // Clicking to navigate
+    //     comp.click().then((a) => {
+    //         // Checking if url is correct
+    //         cy.location("href").should("eq", a.prop("href"));
+    //     });
+    // });
+});
