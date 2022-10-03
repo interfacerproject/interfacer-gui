84b2638d6b7264540ee396e2da2f829a343cb67f
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-10-03 15:25:31 +0200

    Update CreateProjectLayout.cy.tsx

--- cypress/e2e/components/CreateProjectLayout.cy.tsx
+++ cypress/e2e/components/CreateProjectLayout.cy.tsx
@@ -1,10 +1,10 @@
 describe("CreateProjectLayout component", () => {
-    it("should block the user, and display login buttons", () => {
+    it("should block the unlogged user, and display login buttons", () => {
         cy.visit("/create_asset");
         cy.contains("Login").should("be.visible");
     });
 
-    it("should allow the user to /create_asset", () => {
+    it("should allow the logged user to go to /create_asset", () => {
         cy.login();
         cy.visit("/create_asset");
         cy.contains("Create a new asset").should("be.visible");
