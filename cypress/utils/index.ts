36310e3322c8e2ab403c9d130cc307ead7128466
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-10-06 13:47:09 +0200

    test: Assets table test – #76 (#106)
    * cypress tests: added utility to wait for data load
    * cypress tests: added AssetsTable
    * fixed typo
    
    fixes #76 
    
    Co-authored-by: Puria Nafisi Azizi <puria@dyne.org>

--- cypress/utils/index.ts
+++ cypress/utils/index.ts
@@ -1,3 +1,15 @@
+import { RouteMatcher } from "cypress/types/net-stubbing";
+
+export function waitForData(
+    requestUrl: RouteMatcher = "http://65.109.11.42:9000/api",
+    name = "waitForData"
+) {
+    // Intercepting request and naming it
+    cy.intercept(requestUrl).as(name);
+    // Waiting for the request
+    cy.wait(`@${name}`);
+}
+
 export function randomString() {
     return Math.random().toString(32).substring(2);
 }
