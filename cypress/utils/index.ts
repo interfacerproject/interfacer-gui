99c52fa6311b9fed33c22f3f6f8d1ac9090fe43b
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-10-06 08:34:48 +0200

    fixed typo

--- cypress/utils/index.ts
+++ cypress/utils/index.ts
@@ -8,6 +8,7 @@ export function waitForData(
     cy.intercept(requestUrl).as(name);
     // Waiting for the request
     cy.wait(`@${name}`);
+}
 
 export function randomString() {
     return Math.random().toString(32).substring(2);
