d7927afae7a3ff736fdaf43291676e9922f4df03
phoebus-84 <ennio@dyne.org>
2022-10-06 16:28:57 +0200

    translate notification

--- pages/notification.tsx
+++ pages/notification.tsx
@@ -2,6 +2,8 @@ import {gql, useQuery} from "@apollo/client";
 import {useAuth} from "../lib/auth";
 import dayjs from "../lib/dayjs";
 import Link from "next/link";
+import {serverSideTranslations} from "next-i18next/serverSideTranslations";
+import {useTranslation} from "next-i18next";
 
 const QUERY_ASSETS = gql`query ($first: Int, $after: ID, $last: Int, $before: ID, $filter:ProposalFilterParams) {
   proposals(first: $first, after: $after, before: $before, last: $last, filter: $filter) {
@@ -38,21 +40,32 @@ const QUERY_ASSETS = gql`query ($first: Int, $after: ID, $last: Int, $before: ID
 
 const Notification = () => {
     const {authId} = useAuth();
+    const {t} = useTranslation("notificationProps");
     const {data, startPolling} = useQuery(QUERY_ASSETS, {variables: {last: 50}})
     startPolling(4000)
-    const notifications = data?.proposals.edges.filter((proposal: any) => proposal.node.primaryIntents[0]?.resourceInventoriedAs?.metadata?.contributors.some((c: {id:string, name:string}) => c.id === authId))
+    const notifications = data?.proposals.edges.filter((proposal: any) => proposal.node.primaryIntents[0]?.
+    resourceInventoriedAs?.metadata?.contributors.some((c: {id:string, name:string}) => c.id === authId))
     return <div className="grid grid-cols-1 justify-items-center p-12">
         {notifications?.map((n: any) => <div key={n.node.id} className="my-6">
             <b className="mr-1 p-8">{dayjs(n.node.created).fromNow()}</b>
             <Link href={`/profile/${n.node.primaryIntents[0].resourceInventoriedAs.primaryAccountable.id}`}>
-                <a className="text-primary hover:underline">{n.node.primaryIntents[0].resourceInventoriedAs.primaryAccountable.name}</a>
-            </Link> added you as contributor to
+                <a className="text-primary hover:underline">
+                    {n.node.primaryIntents[0].resourceInventoriedAs.primaryAccountable.name}
+                </a>
+            </Link> {t("added")}
             <Link href={`/asset/${n.node.id}`}>
                 <a className="text-primary hover:underline"> {n.node.primaryIntents[0].resourceInventoriedAs.name}</a>
             </Link>
         </div>)}
     </div>
+}
 
+export async function getStaticProps({locale}: any) {
+    return {
+        props: {
+            ...(await serverSideTranslations(locale, ['signInProps', 'notificationProps', 'SideBarProps'])),
+        },
+    };
 }
 
 export default Notification
