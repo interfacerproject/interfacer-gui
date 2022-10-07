e9a69d978080d368ae9691d2bf7ce9af0714053f
Puria Nafisi Azizi <puria@dyne.org>
2022-10-07 17:26:42 +0200

    Test assets creation (#62)
    * Test assets creation
    
    * Skip flaky
    
    * prettier format
    
    * commented test
    
    * prettier format, added testID prop for easier testing
    
    * renamed data-testID in data-test
    
    * prettier format, added data-test
    
    * prettier format, added data-test
    
    * prettier format, added data-test
    
    * added data-test to all fields
    
    * added data-test to submit
    
    * cypress utils: changed randomString function
    
    * edited create_asset test
    
    * fixed import name
    
    * Update create_asset.cy.ts
    
    Co-authored-by: bbtgnn <30571828+bbtgnn@users.noreply.github.com>

--- components/brickroom/BrInput.tsx
+++ components/brickroom/BrInput.tsx
@@ -1,49 +1,55 @@
 import React, { ChangeEventHandler, LegacyRef } from "react";
-import { ExclamationIcon } from '@heroicons/react/solid'
-
+import { ExclamationIcon } from "@heroicons/react/solid";
 
 type BrInputProps = {
-    type?: 'number' | 'text' | 'password' | 'date' | 'email',
-    value?: string,
-    placeholder?: string,
-    label?: string,
-    onChange?: ChangeEventHandler,
-    onBlur?: ChangeEventHandler,
-    hint?: string,
-    error?: string,
-    className?: string,
-    ref?: any,
-    help?: string,
-}
-
+    type?: "number" | "text" | "password" | "date" | "email";
+    value?: string;
+    placeholder?: string;
+    label?: string;
+    onChange?: ChangeEventHandler;
+    onBlur?: ChangeEventHandler;
+    hint?: string;
+    error?: string;
+    className?: string;
+    ref?: any;
+    help?: string;
+    testID?: string;
+};
 
 const BrInput = (props: BrInputProps) => {
-    return (<>
+    return (
+        <>
+            <div className={`form-control ${props.className}`}>
+                <label className="label">
+                    <h4 className="label-text">{props.label}</h4>
+                </label>
+                <input
+                    type={props.type}
+                    placeholder={props.placeholder}
+                    onChange={props.onChange}
+                    onBlur={props.onBlur}
+                    className="w-full rounded-md input input-bordered focus:input-primary"
+                    value={props.value}
+                    ref={props.ref}
+                    data-test={props.testID}
+                />
+                <label className="flex-col items-start label">
+                    {props.error && (
+                        <span className="flex flex-row items-center justify-between label-text-alt text-warning">
+                            <ExclamationIcon className="w-5 h-5" />
+                            {props.error}
+                        </span>
+                    )}
+                    {props.hint && (
+                        <span className="label-text-alt">{props.hint}</span>
+                    )}
+                    {props.help && (
+                        <p className="text-[#8A8E96]">{props.help}</p>
+                    )}
+                </label>
+            </div>
+        </>
+    );
+};
 
-        <div className={`form-control ${props.className}`}>
-            <label className="label">
-                <h4 className="label-text">{props.label}</h4>
-            </label>
-            <input type={props.type}
-                placeholder={props.placeholder}
-                onChange={props.onChange}
-                onBlur={props.onBlur}
-                className="w-full rounded-md input input-bordered focus:input-primary"
-                value={props.value}
-                ref={props.ref}
-            />
-            <label className="flex-col items-start label">
-                {props.error &&
-                    <span className="flex flex-row items-center justify-between label-text-alt text-warning">
-                        <ExclamationIcon className='w-5 h-5' />
-                        {props.error}
-                    </span>
-                }
-                {props.hint && <span className="label-text-alt">{props.hint}</span>}
-                {props.help && <p className="text-[#8A8E96]">{props.help}</p>}
-            </label>
-        </div>
-    </>)
-}
-
-export default BrInput
+export default BrInput;
