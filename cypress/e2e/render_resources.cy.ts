50474a71af7a7f027fbf844ddc2ba8597e0206e0
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-10-12 12:37:45 +0200

    fix(ResourcesTable): 🐛 replaced tr and td with divs (#142)
    * fix(ResourcesTable): 🐛 replaced tr and td with divs
    
    * fix(test): 🐛 🚨 corrected render_resources after <tr> to <div> change

--- cypress/e2e/render_resources.cy.ts
+++ cypress/e2e/render_resources.cy.ts
@@ -1,18 +1,16 @@
+import { waitForData } from "../utils";
+
 describe("When user visit resources", () => {
   before(() => {
     cy.login();
     cy.saveLocalStorage();
   });
 
-  beforeEach(() => {
-    cy.visit("");
-    cy.restoreLocalStorage();
-  });
-
   it("should Filter resources by the url query string", () => {
     cy.visit("/resources?primaryAccountable=061KPJM661MN6S3QA3PPQ6AQDR&conformTo=061KFDE93ARR3Q67J2PMBS0JGC");
-    cy.get("tr").each($tr => {
-      cy.wrap($tr).get("td").eq(0).should("contain", "nenno").and("contain", "Design");
+    waitForData();
+    cy.get(`[data-test="resource-item"]`).each($item => {
+      cy.wrap($item).children(".table-cell").eq(0).should("contain", "nenno").and("contain", "Design");
     });
   });
 });
