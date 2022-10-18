9878eefb51f4a1bcfd8426fa2e657c74b5808bf4
phoebus-84 <83974413+phoebus-84@users.noreply.github.com>
2022-10-18 15:35:30 +0200

    feat: notifications / assets page (#155)
    * refactor: ♻️ remove fab tokens from asset creation
    
    * refactor: ♻️ remove fab tokens from asset detail
    
    * refactor: ♻️ remove fab tokens from assets table
    
    * test: 🚨 test notification and mark as read
    
    * feat: ✨ add thumbinail gallery and tabs in asset details
    
    * style: 🎨 cherry-pick BrBreadcrumb
    
    * style: 🎨 reorganize asset detail
    
    * feat: ✨ add contributors tab
    
    * .gitmodules path do not have to change
    
    * build: 📦️ update zenflows-crypto to latest
    
    * build: 📦️ add missing dependencies
    
    * test: 🚨 fix test
    
    * test: 🚨 remove tag selectors and remove test
    
    Co-authored-by: phoebus-84 <ennio@dyne.org>
    Co-authored-by: Puria Nafisi Azizi <puria@dyne.org>

--- components/NotificationBell.tsx
+++ components/NotificationBell.tsx
@@ -1,9 +1,9 @@
 import { gql, useQuery } from "@apollo/client";
 import { BellIcon } from "@heroicons/react/outline";
 import { useAuth } from "../hooks/useAuth";
-import devLog from "../lib/devLog";
 import Link from "next/link";
 import dayjs from "../lib/dayjs";
+import useStorage from "../hooks/useStorage";
 const QUERY_ASSETS = gql`
   query ($first: Int, $after: ID, $last: Int, $before: ID, $filter: ProposalFilterParams) {
     proposals(first: $first, after: $after, before: $before, last: $last, filter: $filter) {
@@ -36,19 +36,20 @@ const NotificationBell = () => {
   const { user } = useAuth();
   const { data, startPolling } = useQuery(QUERY_ASSETS, { variables: { last: 50 } });
   startPolling(4000);
+  const { getItem } = useStorage();
   const notifications = data?.proposals.edges.filter((proposal: any) =>
     proposal.node.primaryIntents[0]?.resourceInventoriedAs?.metadata?.contributors.some((c: any) => c.id === user?.ulid)
   );
-  const hasNewNotification =
+  const hasIncomingNotification =
     notifications?.length > 0 && dayjs(notifications[0].node.created).fromNow().includes("seconds");
-  devLog("notifications", notifications);
+  const hasNewNotification = notifications?.filter((n: any) => getItem(n.node.id) !== "read").length > 0;
   return (
     <Link href="/notification">
-      <a className="relative mr-4">
+      <a className="relative mr-4" id="notification-bell">
         <button className="bg-white btn btn-circle btn-accent">
-          <BellIcon className={`w-5 h-5 ${hasNewNotification ? "animate-swing origin-top" : ""}`} />
+          <BellIcon className={`w-5 h-5 ${hasIncomingNotification ? "animate-swing origin-top" : ""}`} />
         </button>
-        {notifications?.length > 0 && (
+        {hasNewNotification && (
           <sup className="absolute top-0 right-0 btn btn-active btn-circle btn-success btn-xs">
             {notifications.length}
           </sup>
