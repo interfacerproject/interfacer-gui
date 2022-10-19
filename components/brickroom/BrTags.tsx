9a2c51efd8588f76478974c03446f5fbcb71d277
Puria Nafisi Azizi <puria@dyne.org>
2022-10-19 15:50:55 +0200

    style: 🎨 fix tags (#178)

--- components/brickroom/BrTags.tsx
+++ components/brickroom/BrTags.tsx
@@ -1,29 +1,17 @@
-import React from "react";
 import Link from "next/link";
 
 const BrTags = ({ tags }: { tags?: Array<string>; onCancel?: (tag: string) => void; testID?: string }) => {
-  // @ts-ignore
-  const stringToColour: (str: string) => CSS.Properties = (str: string) => {
-    let hash = str.split("").reduce((pre, cur, index) => {
-      return str.charCodeAt(index) + ((pre << 5) - pre);
-    }, 0);
-    var colour = "#";
-    for (var i = 0; i < 3; i++) {
-      var value = (hash >> (i * 8)) & 0xff;
-      colour += ("00" + value.toString(16)).substr(-2);
-    }
-    return { backgroundColor: colour };
-  };
-
   return (
     <>
-      <div className="hidden badge-neutral badge-error badge-accent badge-primary badge-succes badge-warning" />
       <div className="w-full">
         {tags && tags.length > 0 && (
           <>
             {tags?.map((tag: string, index) => (
               <Link href={`/assets?tags=${tag}`} key={index}>
-                <a key={tag} style={stringToColour(tag)} className={`badge  rounded-md float-left mb-1 mr-1 p-3`}>
+                <a
+                  key={tag}
+                  className={`bg-[#CDE4DF] text-[#5DA091] border-[#5DA091] border border-1 rounded-[4px] text-sm float-left mb-1 mr-1 px-0.5`}
+                >
                   {tag}
                 </a>
               </Link>
