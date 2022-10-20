283f0b30224cfefb37d68750e3d9d5b5a2f7572f
phoebus-84 <ennio@dyne.org>
2022-10-20 15:02:44 +0200

    fix: 🐛 image of claimed asset also in assets table

--- components/AssetsTableRow.tsx
+++ components/AssetsTableRow.tsx
@@ -8,18 +8,24 @@ import AvatarUsers from "./AvatarUsers";
 const AssetsTableRow = (props: any) => {
   const e = props.asset;
   const primaryIntent = e.node.primaryIntents[0];
-  const reciprocalIntent = e.node.reciprocalIntents[0];
   return (
     <>
       {e && primaryIntent && (
         <tr>
           <td>
             <div className="grid grid-col-1 mx-auto md:mx-0 md:flex max-w-xs min-w-[10rem]">
-              {primaryIntent.resourceInventoriedAs?.images[0] && (
-                <div className="flex-none w-full md:w-2/5">
-                  <AssetImage image={primaryIntent.resourceInventoriedAs?.images[0]} className="mr-1 max-h-20" />
-                </div>
-              )}
+              {primaryIntent.resourceInventoriedAs?.images[0] ||
+                (primaryIntent.resourceInventoriedAs.metadata?.image && (
+                  <div className="flex-none w-full md:w-2/5">
+                    <AssetImage
+                      image={
+                        primaryIntent.resourceInventoriedAs?.images[0] ||
+                        primaryIntent.resourceInventoriedAs.metadata?.image[0]
+                      }
+                      className="mr-1 max-h-20"
+                    />
+                  </div>
+                ))}
               <Link href={`/asset/${e.node.id}`} className="flex-auto">
                 <a className="ml-1">
                   <h3 className="break-words whitespace-normal">{primaryIntent.resourceInventoriedAs?.name}</h3>
