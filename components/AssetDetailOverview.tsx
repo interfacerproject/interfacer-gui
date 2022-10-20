adc54f00996a8de309a2b20512b2b6de002bf9da
Puria Nafisi Azizi <puria@dyne.org>
2022-10-20 13:09:24 +0200

    fix: 🐛 Show long readme and typography (#187)
    Fixes #188

--- components/AssetDetailOverview.tsx
+++ components/AssetDetailOverview.tsx
@@ -3,10 +3,10 @@ import { EconomicResource } from "../lib/types";
 import BrTags from "./brickroom/BrTags";
 
 const AssetDetailOverview = ({ asset }: { asset: EconomicResource }) => {
-  const content = asset.note!.split(":")[1]?.split(",")[0] ?? "";
+  const content = asset?.note ? asset.note.split("description:")[1] : "";
   return (
     <>
-      <div className="my-2" dangerouslySetInnerHTML={{ __html: MdParser.render(content) }} />
+      <div className="my-2 prose" dangerouslySetInnerHTML={{ __html: MdParser.render(content) }} />
       <div className="mb-16">
         <BrTags tags={asset?.classifiedAs!} />
       </div>
