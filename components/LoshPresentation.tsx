7b34bec34e8957024142d3af926a14f0b90bcbfa
phoebus-84 <ennio@dyne.org>
2022-10-20 16:55:32 +0200

    style: 🎨 adopt profile style in Losh Presentation

--- components/LoshPresentation.tsx
+++ components/LoshPresentation.tsx
@@ -1,5 +1,11 @@
 import { useTranslation } from "next-i18next";
 import { EconomicResource } from "../lib/types";
+import BrDisplayUser from "./brickroom/BrDisplayUser";
+import { LinkIcon } from "@heroicons/react/outline";
+import Link from "next/link";
+import BrTags from "./brickroom/BrTags";
+import MdParser from "../lib/MdParser";
+import { router } from "next/client";
 
 const LoshPresentation = ({
   economicResource,
@@ -9,37 +15,65 @@ const LoshPresentation = ({
   goToClaim?: () => void;
 }) => {
   const { t } = useTranslation("ResourceProps");
+  const m = economicResource?.metadata;
   return (
     <>
       {economicResource && (
         <div className="grid grid-cols-1 gap-2 md:grid-cols-12 pt-14">
           <div className="md:col-start-2 md:col-end-7">
             <h2>{economicResource.name}</h2>
-            <p className="mb-1 text-gray-500">{economicResource.note}</p>
-            <p className="text-gray-500">
-              This is a{/* {mapUnit(data?.economicResource.onhandQuantity?.hasUnit.label)} */}
-              <span className="text-primary">
-                {economicResource.conformsTo && `${economicResource.conformsTo.name}`}
-              </span>
+            <p className="pt-4 text-gray-500">
+              This is a &nbsp;
+              <Link href={`/resources`}>
+                <a className="text-primary">{t("Losh asset")}</a>
+              </Link>
             </p>
+            <span className="pt-4 text-primary">ID: {economicResource.id}</span>
+            {m && (
+              <>
+                <div className="pt-12 text-primary">
+                  <Link href={m.repo}>
+                    <a target="_blank" className="flex items-center">
+                      <LinkIcon className="h-4" /> &nbsp; {m.repo}
+                    </a>
+                  </Link>
+                </div>
+                <div className="pt-12 prose" dangerouslySetInnerHTML={{ __html: MdParser.render(m.function) }} />
+                <img src={m.image} className="w-full py-10" />
+                <BrTags tags={m.tags} />
+              </>
+            )}
           </div>
 
           <div className="md:col-start-8 md:col-end-13">
-            <div>
-              <h4>{t("assigned to:")}</h4>
-              <p className="text-gray-500">{economicResource.primaryAccountable?.name}</p>
+            <div className="flex flex-col">
+              <span className="font-semibold">{m?.license}</span>
+              <span className="italic text-primary">
+                {t("by")} {m?.licensor}
+              </span>
+
+              <span className="pt-8">
+                {t("Version")}: {m?.version}
+              </span>
+              {m?.okhv}
+
+              {goToClaim && (
+                <>
+                  <button type="button" className="mt-16 mr-8 w-72 btn btn-accent" onClick={goToClaim}>
+                    {t("CLAIM OWNERSHIP")}
+                  </button>
+                  <button type="button" className="mt-3 mr-8 w-72 btn btn-outline">
+                    {t("add to list +")}
+                  </button>
+                </>
+              )}
+              <span className="pt-9">{t("Owner")}</span>
+              <BrDisplayUser
+                id={economicResource.primaryAccountable.id}
+                name={economicResource.primaryAccountable.name}
+                location={economicResource.currentLocation?.name}
+              />
             </div>
-            <div>
-              <h4>{t("current location:")}</h4>
-              <p className="text-gray-500">{economicResource.currentLocation?.name}</p>
-            </div>
-            {goToClaim && (
-              <div className="md:w-3/5 px-0.5 md:px-0">
-                <button className="px-20 mb-4 btn btn-accent btn-block" onClick={goToClaim}>
-                  {t("Claim Ownership")}
-                </button>
-              </div>
-            )}
           </div>
         </div>
       )}
