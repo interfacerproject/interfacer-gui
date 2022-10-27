473c0f2d4fdabde2689315d0a166cef679256627
phoebus-84 <ennio@dyne.org>
2022-10-27 15:43:55 +0200

    feat: ✨ add useInBox hook

---
+++ hooks/useInBox.ts
@@ -0,0 +1,61 @@
+import { useAuth } from "./useAuth";
+import dayjs from "dayjs";
+import { zencode_exec } from "zenroom";
+import sign from "../zenflows-crypto/src/sign_graphql";
+import useStorage from "./useStorage";
+
+type UseInBoxReturnValue = {
+  sendMessage: (message: string, receivers: string[], subject: string) => Promise<Response>;
+  readMessages: () => Promise<Response>;
+};
+
+const useInBox = (): UseInBoxReturnValue => {
+  const { user } = useAuth();
+  const { getItem } = useStorage();
+  const signRequest = async (json: string) => {
+    const data = `{"gql": "${Buffer.from(json, "utf8").toString("base64")}"}`;
+    const keys = `{"keyring": {"eddsa": "${getItem("eddsa_key")}"}}`;
+    const { result } = await zencode_exec(sign(), { data, keys });
+    return {
+      "zenflows-sign": JSON.parse(result).eddsa_signature,
+    };
+  };
+
+  const sendMessage = async (message: string, receivers: string[], subject: string = "Subject"): Promise<Response> => {
+    const request = {
+      sender: user?.email,
+      receiver: receivers,
+      message: message,
+      subject: subject,
+      data: dayjs(),
+    };
+    const requestJSON = JSON.stringify(request);
+    const requestHeaders = await signRequest(requestJSON);
+
+    const options = {
+      method: "POST",
+      body: JSON.stringify(request),
+      headers: requestHeaders,
+    };
+    return await fetch(process.env.NEXT_PUBLIC_INBOX_SEND!, options);
+  };
+
+  const readMessages = async () => {
+    const request = {
+      request_id: 42,
+      sender: user?.email,
+    };
+    const requestJSON = JSON.stringify(request);
+    const requestHeaders = await signRequest(requestJSON);
+    const options = {
+      method: "POST",
+      body: JSON.stringify(request),
+      headers: requestHeaders,
+    };
+    return await fetch(process.env.NEXT_PUBLIC_INBOX_READ!, options);
+  };
+
+  return { sendMessage, readMessages };
+};
+
+export default useInBox;
