d24c5a6fa11fd23ddc5571db9386dd04be346405
phoebus-84 <ennio@dyne.org>
2022-11-01 18:34:08 +0100

    refactor: ♻️ notification bell use useInBox

--- components/NotificationBell.tsx
+++ components/NotificationBell.tsx
@@ -1,60 +1,40 @@
-import { gql, useQuery } from "@apollo/client";
 import { BellIcon } from "@heroicons/react/outline";
 import Link from "next/link";
-import { useAuth } from "../hooks/useAuth";
-import useStorage from "../hooks/useStorage";
-import dayjs from "../lib/dayjs";
-const QUERY_ASSETS = gql`
-  query ($first: Int, $after: ID, $last: Int, $before: ID, $filter: ProposalFilterParams) {
-    proposals(first: $first, after: $after, before: $before, last: $last, filter: $filter) {
-      pageInfo {
-        startCursor
-        endCursor
-        hasPreviousPage
-        hasNextPage
-        totalCount
-        pageLimit
-      }
-      edges {
-        cursor
-        node {
-          id
-          name
-          created
-          primaryIntents {
-            resourceInventoriedAs {
-              metadata
-            }
-          }
-        }
-      }
-    }
-  }
-`;
+import useInBox from "../hooks/useInBox";
+import { useEffect, useState } from "react";
 
 const NotificationBell = () => {
-  const { user } = useAuth();
-  const { data, startPolling } = useQuery(QUERY_ASSETS, { variables: { last: 50 } });
-  startPolling(120000);
-  const { getItem } = useStorage();
-  const notifications = data?.proposals.edges.filter((proposal: any) =>
-    proposal.node.primaryIntents[0]?.resourceInventoriedAs?.metadata?.contributors?.some(
-      (c: any) => c.id === user?.ulid
-    )
-  );
-  const hasIncomingNotification =
-    notifications?.length > 0 && dayjs(notifications[0].node.created).fromNow().includes("seconds");
-  const hasNewNotification = notifications?.filter((n: any) => getItem(n.node.id) !== "read").length > 0;
+  const { countMessages } = useInBox();
+  const [unreadLength, setUnreadLength] = useState(0);
+  const [swinging, setSwinging] = useState(false);
+  const count = async () => {
+    const _count = await countMessages();
+    if (_count.success) {
+      return _count.count;
+    }
+  };
+  useEffect(() => {
+    setInterval(() => {
+      const previousCounted = unreadLength;
+      count().then(counted => {
+        if (previousCounted < counted) {
+          setSwinging(true);
+          setUnreadLength(counted);
+          setInterval(() => {
+            setSwinging(false);
+          }, 10000);
+        }
+      });
+    }, 120000);
+  }, []);
   return (
     <Link href="/notification">
       <a className="relative mr-4" id="notification-bell">
         <button className="bg-white btn btn-circle btn-accent">
-          <BellIcon className={`w-5 h-5 ${hasIncomingNotification ? "animate-swing origin-top" : ""}`} />
+          <BellIcon className={`w-5 h-5 ${swinging ? "animate-swing origin-top" : ""}`} />
         </button>
-        {hasNewNotification && (
-          <sup className="absolute top-0 right-0 btn btn-active btn-circle btn-success btn-xs">
-            {notifications.length}
-          </sup>
+        {unreadLength > 0 && (
+          <sup className="absolute top-0 right-0 btn btn-active btn-circle btn-success btn-xs">{unreadLength}</sup>
         )}
       </a>
     </Link>
--- hooks/useInBox.ts
+++ hooks/useInBox.ts
@@ -7,6 +7,7 @@ import useStorage from "./useStorage";
 type UseInBoxReturnValue = {
   sendMessage: (message: any, receivers: string[], subject: string) => Promise<Response>;
   readMessages: () => Promise<{ messages?: any; request_id: number; success: boolean }>;
+  countMessages: () => Promise<any>;
 };
 
 const useInBox = (): UseInBoxReturnValue => {
@@ -20,6 +21,15 @@ const useInBox = (): UseInBoxReturnValue => {
       "zenflows-sign": JSON.parse(result).eddsa_signature,
     };
   };
+  const post = async (url: string, request: any) => {
+    const requestJSON = JSON.stringify(request);
+    const requestHeaders = await signRequest(requestJSON);
+    return await fetch(url, {
+      method: "POST",
+      headers: requestHeaders,
+      body: JSON.stringify(request),
+    });
+  };
 
   const sendMessage = async (message: any, receivers: string[], subject: string = "Subject"): Promise<Response> => {
     const request = {
@@ -29,15 +39,7 @@ const useInBox = (): UseInBoxReturnValue => {
       subject: subject,
       data: dayjs(),
     };
-    const requestJSON = JSON.stringify(request);
-    const requestHeaders = await signRequest(requestJSON);
-
-    const options = {
-      method: "POST",
-      body: JSON.stringify(request),
-      headers: requestHeaders,
-    };
-    return await fetch(process.env.NEXT_PUBLIC_INBOX_SEND!, options);
+    return await post(process.env.NEXT_PUBLIC_INBOX_SEND!, request);
   };
 
   const readMessages = async () => {
@@ -45,17 +47,17 @@ const useInBox = (): UseInBoxReturnValue => {
       request_id: 42,
       receiver: user?.ulid,
     };
-    const requestJSON = JSON.stringify(request);
-    const requestHeaders = await signRequest(requestJSON);
-    const options = {
-      method: "POST",
-      body: JSON.stringify(request),
-      headers: requestHeaders,
-    };
-    return await fetch(process.env.NEXT_PUBLIC_INBOX_READ!, options).then(res => res.json());
+    return await post(process.env.NEXT_PUBLIC_INBOX_READ!, request).then(res => res.json());
   };
 
-  return { sendMessage, readMessages };
+  const countMessages = async () => {
+    const request = {
+      receiver: user?.ulid,
+    };
+    return await post(process.env.NEXT_PUBLIC_INBOX_COUNT_UNREAD!, request).then(res => res.json());
+  };
+
+  return { sendMessage, readMessages, countMessages };
 };
 
 export default useInBox;
