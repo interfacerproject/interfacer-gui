aaa10d649a9d1a6ddfed8358834ca2e2d649b244
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-11-07 08:31:10 +0100

    refactor: ♻️ BrImageUpload and related

--- components/brickroom/BrFieldInfo.tsx
+++ components/brickroom/BrFieldInfo.tsx
@@ -1,13 +1,12 @@
-import { ExclamationIcon } from "@heroicons/react/solid";
-import { ChildrenComponent } from "./types";
+import BrFieldError from "./BrFieldError";
+import { ChildrenComponent, TestProp } from "./types";
 
-export interface BrFieldInfoProps {
+export interface BrFieldInfoProps extends TestProp {
   for?: string;
   label?: string;
   hint?: string;
   error?: string | any;
   help?: string;
-  testID?: string;
 }
 
 export default function BrFieldInfo(props: ChildrenComponent<BrFieldInfoProps>) {
@@ -20,7 +19,7 @@ export default function BrFieldInfo(props: ChildrenComponent<BrFieldInfoProps>) 
   const helpID = testID + "-help";
 
   return (
-    <div className="flex flex-col items-start justify-start space-y-2">
+    <div className="flex flex-col items-stretch justify-start space-y-2">
       {/* Label */}
       {props.label && (
         <label className="" htmlFor={props.for} data-test={labelID}>
@@ -34,14 +33,6 @@ export default function BrFieldInfo(props: ChildrenComponent<BrFieldInfoProps>) 
       {/* Info under field */}
       {(error || hint || help) && (
         <label htmlFor={props.for} className="space-y-2">
-          {/* Error */}
-          {error && (
-            <span className="flex flex-row text-warning pt-1" data-test={errorID}>
-              <ExclamationIcon className="w-5 h-5" />
-              <p className="ml-2 label-text-alt text-warning">{error}</p>
-            </span>
-          )}
-
           {/* Hints */}
           {hint && (
             <span className="label-text-alt" data-test={hintID}>
@@ -55,6 +46,9 @@ export default function BrFieldInfo(props: ChildrenComponent<BrFieldInfoProps>) 
               {help}
             </p>
           )}
+
+          {/* Error */}
+          {error && <BrFieldError testID={errorID} message={error} />}
         </label>
       )}
     </div>
---
+++ components/brickroom/BrImageUploadEmptyState.tsx
@@ -0,0 +1,29 @@
+import { useTranslation } from "next-i18next";
+
+export default function BrImageUploadEmptyState() {
+  const { t } = useTranslation("BrImageUploadProps");
+
+  return (
+    <div className="flex flex-col items-center justify-center space-y-2">
+      <svg
+        aria-hidden="true"
+        className="w-10 h-10 text-gray-400"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+        />
+      </svg>
+
+      <p className="text-sm text-gray-500">
+        <span className="font-semibold">{t("dragHereOrClick")}</span>
+      </p>
+    </div>
+  );
+}
