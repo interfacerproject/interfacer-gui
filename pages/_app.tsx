35f180c86094fabb1f14589876a82c74b4b2639e
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-11-14 19:36:55 +0100

    Add polaris (#243)
    * refactor(auth): ♻️ updated login mutation
    
    * refactor(createAsset): ♻️ moved components to partials
    
    * refactor(createAsset): ♻️ moved components to partials
    
    * refactor: ♻️ BrImageUpload and related (#230)
    
    * refactor(BrMdEditor): ♻️ added BrFieldInfo
    
    * refactor(TagsGeoContributors): ♻️ exporting Contributor interface
    
    * refactor(createAsset): ♻️ renamed NewAssetForm and added comps
    
    * fix(createAsset): 🐛 reimported BrMdEditor
    
    * fix(BrImageUpload): 🐛 missing file previews
    
    * style: 🎨 renamed some imports
    
    * Br radio rework (#234)
    
    * fix(BrRadio): 🐛 removed unused SelectAssetTypeRadio
    
    * refactor: ♻️ moved SelectAssetTypeRadio query to lib and regen types
    
    * refactor(BrRadio): ♻️ added forwardRef
    
    * refactor(searchableSelect): ♻️ improved react-select usage
    
    * refactor: ♻️ improved "interfacer-specific" select components
    
    * fix: 🐛 commented broken parts of old interfacer-specific select components
    
    * refactor(CreateAssetForm): ♻️ applied new select components
    
    * refactor(Filters): ♻️ rename and applied new select components, improved logic
    
    * refactor(createAsset): ♻️ changed controlwindow position after form change
    
    * refactor(AssetsTable): ♻️ removed unused logic after filters refactor
    
    * fix(SelectLocation): 🐛 added back coordinates fetch
    
    * feat: ✨ added AssetTypes formatting
    
    * fix: 🐛 improved data-test on some components
    
    * feat(AssetsFilters): ✨ added testID to fields
    
    * refactor(CreateAsset): ♻️ moved logic from component to page
    
    * refactor(AssetsTable): ♻️ created AssetsTableBase
    
    * feat: ✨ added Polaris design system

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
-import Head from "next/head";
-import "../styles/globals.scss";
-import type { AppProps } from "next/app";
-import { AuthProvider } from "../contexts/AuthContext";
-import Layout from "../components/layout/Layout";
-import { appWithTranslation } from "next-i18next";
+import Layout from "components/layout/Layout";
+import { AuthProvider } from "contexts/AuthContext";
 import { NextPage } from "next";
+import { appWithTranslation } from "next-i18next";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 import { ReactElement, ReactNode } from "react";
+import "styles/globals.scss";
+
+import { AppProvider } from "@bbtgnn/polaris-interfacer";
+import enTranslations from "@bbtgnn/polaris-interfacer/locales/en.json";
+
+//
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -21,12 +26,12 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout || ((page: ReactElement) => <Layout>{page}</Layout>);
   const publicPage = Component.publicPage || false;
   return (
-    <>
+    <AppProvider i18n={enTranslations}>
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <AuthProvider publicPage={publicPage}>{getLayout(<Component {...pageProps} />)}</AuthProvider>
-    </>
+    </AppProvider>
   );
 }
 
