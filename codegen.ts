7ddd623f04ed6c4990950ba72784a681e7764e0e
phoebus-84 <ennio@dyne.org>
2022-12-14 11:03:16 +0100

    fix: 🐛 rreplace test instance address with domani name in codegen.ts

--- codegen.ts
+++ codegen.ts
@@ -1,7 +1,7 @@
 import { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-  schema: "http://65.109.11.42:9000/play",
+  schema: "https://zenflows-test.interfacer.dyne.org/play",
   documents: ["./lib/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
   generates: {
     "./lib/types/index.ts": {
