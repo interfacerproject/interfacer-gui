0dbd8a15daef3c641af55c643e3ae28b80ef6ff7
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-12-14 17:21:29 +0100

    refactor: ♻️ added guard clause to simplify indentation

--- pages/asset/[id].tsx
+++ pages/asset/[id].tsx
@@ -1,6 +1,9 @@
 import { useQuery } from "@apollo/client";
+import { Button } from "@bbtgnn/polaris-interfacer";
 import cn from "classnames";
+import { GetStaticPaths } from "next";
 import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
@@ -10,17 +13,13 @@ import BrBreadcrumb from "../../components/brickroom/BrBreadcrumb";
 import BrDisplayUser from "../../components/brickroom/BrDisplayUser";
 import BrTabs from "../../components/brickroom/BrTabs";
 import BrThumbinailsGallery from "../../components/brickroom/BrThumbinailsGallery";
-import ContributorsTable from "../../components/ContributorsTable";
+import Dpp from "../../components/Dpp";
+import WatchButton from "../../components/WatchButton";
 import { useAuth } from "../../hooks/useAuth";
 import useStorage from "../../hooks/useStorage";
-import { EconomicResource } from "../../lib/types";
-import WatchButton from "../../components/WatchButton";
-import { QUERY_RESOURCE } from "../../lib/QueryAndMutation";
-import { Button } from "@bbtgnn/polaris-interfacer";
-import { GetStaticPaths } from "next";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import devLog from "../../lib/devLog";
-import Dpp from "../../components/Dpp";
+import { QUERY_RESOURCE } from "../../lib/QueryAndMutation";
+import { EconomicResource } from "../../lib/types";
 
 const Asset = () => {
   const { getItem, setItem } = useStorage();
@@ -70,88 +69,86 @@ const Asset = () => {
     setInList(_listParsed.includes(asset?.id));
   }, [asset, getItem]);
 
+  if (!asset) return null;
+
   return (
     <>
-      {asset && (
-        <>
-          <div className="w-full p-2 md:p-8 flex flex-row">
-            <div className="flex-grow">
-              <BrBreadcrumb
-                crumbs={[
-                  { name: t("assets"), href: "/assets" },
-                  { name: asset.conformsTo.name, href: `/assets?conformTo=${asset.conformsTo.id}` },
-                ]}
-              />
-            </div>
-            <AddStar
-              id={asset.id}
-              metadata={asset.metadata}
-              userId={user?.ulid}
-              onStarred={refetch}
-              onDestarred={refetch}
+      <div className="w-full p-2 md:p-8 flex flex-row">
+        <div className="flex-grow">
+          <BrBreadcrumb
+            crumbs={[
+              { name: t("assets"), href: "/assets" },
+              { name: asset.conformsTo.name, href: `/assets?conformTo=${asset.conformsTo.id}` },
+            ]}
+          />
+        </div>
+        <AddStar
+          id={asset.id}
+          metadata={asset.metadata}
+          userId={user?.ulid}
+          onStarred={refetch}
+          onDestarred={refetch}
+        />
+      </div>
+      <div className="grid grid-cols-1 px-2 md:grid-cols-3 md:gap-4 md:px-0 md:mx-32">
+        <div id="left-col" className="flex flex-col col-span-2">
+          <div className="flex flex-col content-end mb-4">
+            <p>
+              {t("This is a")}
+              <Link href={`/assets?conformTo=${asset.conformsTo.id}`}>
+                <a className="ml-1 font-bold text-primary hover:underline">{asset.conformsTo.name}</a>
+              </Link>
+            </p>
+            <h2 className="my-2">{asset.name}</h2>
+            <p className="text-primary">
+              {"ID:"} {asset.id}
+            </p>
+          </div>
+          {images && <BrThumbinailsGallery images={images} />}
+          <div id="tabs" className="my-6">
+            <BrTabs
+              tabsArray={[
+                { title: t("Overview"), component: <AssetDetailOverview asset={asset} /> },
+                // {
+                //   title: t("Contributions"),
+                //   component: (
+                //     <ContributorsTable
+                //       contributors={asset.metadata?.contributors}
+                //       title={t("Contributors")}
+                //       // @ts-ignore
+                //       data={asset.trace?.filter((t: any) => !!t.hasPointInTime)[0].hasPointInTime}
+                //     />
+                //   ),
+                // },
+                { title: t("DPP"), component: <Dpp dpp={data?.economicResource.traceDpp} /> },
+              ]}
             />
           </div>
-          <div className="grid grid-cols-1 px-2 md:grid-cols-3 md:gap-4 md:px-0 md:mx-32">
-            <div id="left-col" className="flex flex-col col-span-2">
-              <div className="flex flex-col content-end mb-4">
-                <p>
-                  {t("This is a")}
-                  <Link href={`/assets?conformTo=${asset.conformsTo.id}`}>
-                    <a className="ml-1 font-bold text-primary hover:underline">{asset.conformsTo.name}</a>
-                  </Link>
-                </p>
-                <h2 className="my-2">{asset.name}</h2>
-                <p className="text-primary">
-                  {"ID:"} {asset.id}
-                </p>
-              </div>
-              {images && <BrThumbinailsGallery images={images} />}
-              <div id="tabs" className="my-6">
-                <BrTabs
-                  tabsArray={[
-                    { title: t("Overview"), component: <AssetDetailOverview asset={asset} /> },
-                    // {
-                    //   title: t("Contributions"),
-                    //   component: (
-                    //     <ContributorsTable
-                    //       contributors={asset.metadata?.contributors}
-                    //       title={t("Contributors")}
-                    //       // @ts-ignore
-                    //       data={asset.trace?.filter((t: any) => !!t.hasPointInTime)[0].hasPointInTime}
-                    //     />
-                    //   ),
-                    // },
-                    { title: t("DPP"), component: <Dpp dpp={data?.economicResource.traceDpp} /> },
-                  ]}
-                />
-              </div>
-            </div>
-            <div id="right-col" className="flex flex-col mt-16">
-              <button
-                className={cn("px-20 mb-4 btn btn-block", {
-                  "btn-accent": !inList,
-                  "btn-outline": inList,
-                })}
-                onClick={handleCollect}
-              >
-                {t(inList ? "remove from list" : "add to list")}
-              </button>
-              <WatchButton id={asset.id} metadata={asset.metadata} />
-              <p className="mt-8 mb-2">{t("Owner")}:</p>
-              <BrDisplayUser
-                id={asset.primaryAccountable.id}
-                name={asset.primaryAccountable.name}
-                location={asset.currentLocation?.name}
-              />
-              <div className="mt-8">
-                <Button size="large" fullWidth primary onClick={() => router.push(`/create/contribution/${id}`)}>
-                  {t("Propose a contribute")}
-                </Button>
-              </div>
-            </div>
+        </div>
+        <div id="right-col" className="flex flex-col mt-16">
+          <button
+            className={cn("px-20 mb-4 btn btn-block", {
+              "btn-accent": !inList,
+              "btn-outline": inList,
+            })}
+            onClick={handleCollect}
+          >
+            {t(inList ? "remove from list" : "add to list")}
+          </button>
+          <WatchButton id={asset.id} metadata={asset.metadata} />
+          <p className="mt-8 mb-2">{t("Owner")}:</p>
+          <BrDisplayUser
+            id={asset.primaryAccountable.id}
+            name={asset.primaryAccountable.name}
+            location={asset.currentLocation?.name}
+          />
+          <div className="mt-8">
+            <Button size="large" fullWidth primary onClick={() => router.push(`/create/contribution/${id}`)}>
+              {t("Propose a contribute")}
+            </Button>
           </div>
-        </>
-      )}
+        </div>
+      </div>
     </>
   );
 };
