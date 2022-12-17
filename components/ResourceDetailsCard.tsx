45f2d71503124244c4feedbe87246c847e0d9ea2
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-12-17 08:52:40 +0100

    feat(resourceCard): ✨ added owner

--- components/ResourceDetailsCard.tsx
+++ components/ResourceDetailsCard.tsx
@@ -1,6 +1,7 @@
-import { Card, Icon, Stack, Tag, Text } from "@bbtgnn/polaris-interfacer";
+import { Card, Icon, Tag, Text } from "@bbtgnn/polaris-interfacer";
 import { DynamicSourceMinor } from "@shopify/polaris-icons";
 import { EconomicResource } from "lib/types";
+import { useTranslation } from "next-i18next";
 
 //
 
@@ -12,6 +13,7 @@ export interface Props {
 
 const ResourceDetailsCard = (props: Props) => {
   const { resource } = props;
+  const { t } = useTranslation("common");
 
   if (!resource) return null;
 
@@ -24,21 +26,29 @@ const ResourceDetailsCard = (props: Props) => {
   }
 
   const type = resource.conformsTo?.name;
+  const owner = resource.primaryAccountable?.name;
 
   return (
     <Card>
-      <div className="p-4 flex flex-row items-center space-x-4">
+      <div className="p-4 flex flex-row items-start space-x-4">
         <div className="w-20 h-20 rounded-md bg-gray-200 overflow-hidden flex items-center justify-center">
           {image && <img src={image} alt="" className="w-full h-full object-cover" />}
           {!image && <Icon color="subdued" source={DynamicSourceMinor} />}
         </div>
 
-        <Stack vertical spacing="extraTight">
+        <div className="grow">
           <Text as="h4" variant="headingMd" fontWeight="bold">
             {resource.name}
           </Text>
-          {type && <Tag>{resource.conformsTo?.name}</Tag>}
-        </Stack>
+          {owner && (
+            <Text as="p" variant="bodyMd">
+              <span className="after:content-[':_']">{t("Owner")}</span>
+              <span className="font-mono font-bold">{owner}</span>
+            </Text>
+          )}
+        </div>
+
+        {type && <Tag>{resource.conformsTo?.name}</Tag>}
       </div>
     </Card>
   );
