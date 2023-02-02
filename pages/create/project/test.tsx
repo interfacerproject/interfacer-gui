b2dd613b9b7e8b9874c5b066972a071621d5b977
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-02-02 10:47:22 +0100

    feat(ProjectCreation): ✨ added base ImagesStep

--- pages/create/project/test.tsx
+++ pages/create/project/test.tsx
@@ -1,7 +1,8 @@
 import Layout from "components/layout/Layout";
-import RelationsStep from "components/partials/create/project/RelationsStep";
-import DeclarationsStep from "components/partials/create/project/DeclarationsStep";
 import ContributorsStep from "components/partials/create/project/ContributorsStep";
+import DeclarationsStep from "components/partials/create/project/DeclarationsStep";
+import ImagesStep from "components/partials/create/project/ImagesStep";
+import RelationsStep from "components/partials/create/project/RelationsStep";
 import { NextPageWithLayout } from "pages/_app";
 import { ReactElement } from "react";
 
@@ -10,6 +11,7 @@ import { ReactElement } from "react";
 const CreateTest: NextPageWithLayout = () => {
   return (
     <>
+      <ImagesStep />
       <RelationsStep />
       <DeclarationsStep />
       <ContributorsStep />
