a712284f8310ef09bb2a10811483baf9993a204e
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-02-02 16:10:30 +0100

    feat(ImagesStep): ✨ completed page

--- components/partials/create/project/ImagesStep.tsx
+++ components/partials/create/project/ImagesStep.tsx
@@ -1,8 +1,25 @@
+import { Stack } from "@bbtgnn/polaris-interfacer";
 import PFileUpload from "components/polaris/PFileUpload";
+import PTitleSubtitle from "components/polaris/PTitleSubtitle";
 import { useTranslation } from "next-i18next";
 
 export default function ImagesStep() {
   const { t } = useTranslation();
 
-  return <PFileUpload></PFileUpload>;
+  return (
+    <Stack vertical spacing="extraLoose">
+      <PTitleSubtitle
+        title={t("Upload pictures")}
+        subtitle={t("Upload pictures to be displayed inside the project page")}
+      />
+      <PFileUpload
+        maxFiles={10}
+        onUpdate={files => {
+          console.log(files);
+        }}
+        maxSize={2000000}
+        helpText={t("Max 10 images | Max file size 2MB | Accepted formats: JPG, PNG, GIF, SVG")}
+      />
+    </Stack>
+  );
 }
