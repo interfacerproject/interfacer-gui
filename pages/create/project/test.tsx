c5f5538b0f46d57aeae1e8ba8be980c266f8672d
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2023-02-03 09:46:24 +0100

    feat: Project creation – Images component (#367)
    * feat(ProjectCreation): ✨ added base ImagesStep
    
    * feat(ImagesStep): ✨ created reusable PFileUpload
    
    * feat(PFileUpload): ✨ added props
    
    * feat(ImagesStep): ✨ completed page
    
    * fix(PFileUpload): 🐛 fixed max files logic
    
    ---------
    
    Co-authored-by: Puria Nafisi Azizi <puria@dyne.org>

--- pages/create/project/test.tsx
+++ pages/create/project/test.tsx
@@ -1,8 +1,9 @@
 import Layout from "components/layout/Layout";
-import LocationStep from "components/partials/create/project/LocationStep";
-import RelationsStep from "components/partials/create/project/RelationsStep";
-import DeclarationsStep from "components/partials/create/project/DeclarationsStep";
 import ContributorsStep from "components/partials/create/project/ContributorsStep";
+import DeclarationsStep from "components/partials/create/project/DeclarationsStep";
+import ImagesStep from "components/partials/create/project/ImagesStep";
+import RelationsStep from "components/partials/create/project/RelationsStep";
+import LocationStep from "components/partials/create/project/LocationStep";
 import LicenseStep from "components/partials/create/project/LicenseStep";
 import { NextPageWithLayout } from "pages/_app";
 import { ReactElement } from "react";
@@ -12,6 +13,7 @@ import { ReactElement } from "react";
 const CreateTest: NextPageWithLayout = () => {
   return (
     <div className="space-y-12">
+      <ImagesStep />
       <LicenseStep />
       <LocationStep projectType="product" />
       <RelationsStep />
