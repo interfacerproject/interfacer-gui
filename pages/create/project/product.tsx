31ac21cc47c1d5a3e345184d513f1651dca26df3
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2023-02-03 11:27:00 +0100

    feat: Project creation – Home page (#369)
    * feat(CreateProject): ✨ added main page draft
    
    * feat(CreateProject): ✨ completed home
    
    * feat(CreateProject): ✨ created draft forms

---
+++ pages/create/project/product.tsx
@@ -0,0 +1,34 @@
+import Layout from "components/layout/Layout";
+import ContributorsStep from "components/partials/create/project/ContributorsStep";
+import DeclarationsStep from "components/partials/create/project/DeclarationsStep";
+import LicenseStep from "components/partials/create/project/LicenseStep";
+import LocationStep from "components/partials/create/project/LocationStep";
+import RelationsStep from "components/partials/create/project/RelationsStep";
+import { NextPageWithLayout } from "pages/_app";
+import { ReactElement } from "react";
+
+//
+
+const CreateProduct: NextPageWithLayout = () => {
+  return (
+    <div className="space-y-12">
+      <LicenseStep />
+      <LocationStep projectType="product" />
+      <RelationsStep />
+      <DeclarationsStep />
+      <ContributorsStep />
+    </div>
+  );
+};
+
+//
+
+CreateProduct.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <Layout>
+      <div className="max-w-xl mx-auto p-4">{page}</div>
+    </Layout>
+  );
+};
+
+export default CreateProduct;
