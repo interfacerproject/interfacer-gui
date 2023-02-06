761f020fd1bc6cd402fe30e3ef2b5693d5d9976e
phoebus-84 <ennio@dyne.org>
2023-02-06 09:01:17 +0100

    feat: watch button as follow in activityPub

--- hooks/useSocial.ts
+++ hooks/useSocial.ts
@@ -1,3 +1,4 @@
+import devLog from "lib/devLog";
 import { useEffect, useState } from "react";
 import { useAuth } from "./useAuth";
 import useSignedPost from "./useSignedPost";
@@ -7,47 +8,85 @@ type UseSocialReturnValue = {
   isLiked: (economicResource: string) => boolean;
 };
 
-const useSocial = () => {
+const useSocial = (economicResource?: string) => {
   const [likes, setLikes] = useState<string[]>([]);
-  const [liked, setLiked] = useState<any>(null);
+  const [flag, setFlag] = useState<number>(0);
+  const [userFollows, setUserFollows] = useState<string[]>([]);
+  const [erFollows, setERFollows] = useState<string[]>([]);
   const { signedPost } = useSignedPost();
   const { user } = useAuth();
 
   useEffect(() => {
     if (!user) return;
     getLikes();
-  }, [user, liked]);
+    getUserFollows();
+    if (!economicResource) return;
+    getERFollows();
+  }, [user, flag]);
 
   const getLikes = async () => {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_SOCIAL}/${user!.ulid}/liked`);
+    const response = await fetch(`${process.env.NEXT_PUBLIC_SOCIAL_PERSON}/${user!.ulid}/liked`);
     const data = await response.json();
     var _likes: string[] = [];
     for (const item of data.data.items) {
       const _like = await fetch(item);
       const _likeData = await _like.json();
-      _likes.push(_likeData.data.object.split("economicresource/")[1].split("%")[0]);
+      _likes.push(_likeData.data.object.split("economicresource/")[1]);
     }
     setLikes(_likes);
   };
 
-  const likeObject = async (economicResource: string): Promise<any> => {
+  const getERFollows = async () => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_SOCIAL_ECONOMIC_RESOURCE}/${economicResource}/follower`);
+    const data = await response.json();
+    setERFollows(data.data.map((f:string) => f.split("person/")[1]));
+    devLog("erFollows");
+    return data.data;
+  };
+
+  const getUserFollows = async () => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_SOCIAL_PERSON}/${user!.ulid}/following`);
+    const data = await response.json();
+    const followers = data.data.map((f:string) => f.split("economicresource/")[1]);
+    setUserFollows(followers);
+    return data.data;
+  };
+
+  const likeER = async (): Promise<any> => {
     const data = new Date().toISOString();
     const request = {
       "@context": "https://www.w3.org/ns/activitystreams",
       type: "Like",
-      actor: `${process.env.NEXT_PUBLIC_SOCIAL}/${user!.ulid}`,
-      object: `${process.env.NEXT_PUBLIC_SOCIAL_ECONOMIC_RESOURCE}/${economicResource}}`,
+      actor: `${process.env.NEXT_PUBLIC_SOCIAL_PERSON}/${user!.ulid}`,
+      object: `${process.env.NEXT_PUBLIC_SOCIAL_ECONOMIC_RESOURCE}/${economicResource}`,
       published: data,
     };
-    const result = await signedPost(`${process.env.NEXT_PUBLIC_SOCIAL}/${user!.ulid}/outbox`, request);
-    setLiked(result);
+    await signedPost(`${process.env.NEXT_PUBLIC_SOCIAL_PERSON}/${user!.ulid}/outbox`, request);
+    setFlag(flag + 1);
+  };
+
+  const followActivity = async (): Promise<any> => {
+    const data = new Date().toISOString();
+    const request = {
+      "@context": "https://www.w3.org/ns/activitystreams",
+      type: "Follow",
+      actor: `${process.env.NEXT_PUBLIC_SOCIAL_PERSON}/${user!.ulid}`,
+      object: `${process.env.NEXT_PUBLIC_SOCIAL_ECONOMIC_RESOURCE}/${economicResource}`,
+      published: data,
+    };
+    await signedPost(`${process.env.NEXT_PUBLIC_SOCIAL_PERSON}/${user!.ulid}/outbox`, request);
+    setFlag(flag + 1);
   };
 
   const isLiked = (economicResource: string): boolean => likes?.includes(economicResource);
-
+  const isFollowed = (): boolean => erFollows?.includes(user!.ulid);
   return {
-    likeObject,
+    likeER,
+    followActivity,
     isLiked,
+    isWatched: isFollowed,
+    getERFollows,
+    getUserFollows,
   };
 };
 
