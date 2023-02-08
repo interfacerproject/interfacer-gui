74fc49a542be32217ad2889d3a2e76285399c4e9
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-02-08 12:09:06 +0100

    fix(RelationsStep): 🐛 fixed form logic and display

--- components/ProjectDisplay.tsx
+++ components/ProjectDisplay.tsx
@@ -1,35 +1,63 @@
+import { gql, useQuery } from "@apollo/client";
 import { Tag, Text } from "@bbtgnn/polaris-interfacer";
-import { EconomicResource } from "lib/types";
+import { EconomicResource, SearchProjectQuery, SearchProjectQueryVariables } from "lib/types";
 import ProjectThumb from "./ProjectThumb";
 
 export interface Props {
-  project: Partial<EconomicResource>;
+  project?: Partial<EconomicResource>;
+  projectId?: string;
 }
 
 export default function ProjectDisplay(props: Props) {
-  const { project } = props;
+  const { project, projectId } = props;
+
+  let p: Partial<EconomicResource>;
+
+  const { data } = useQuery<SearchProjectQuery, SearchProjectQueryVariables>(SEARCH_PROJECT, {
+    variables: { id: projectId! },
+    skip: !projectId,
+  });
+
+  if (project) p = project;
+  else if (projectId && data?.economicResource) p = data?.economicResource as Partial<EconomicResource>;
+  else return <></>;
 
   return (
     <div className="flex flex-row">
-      <ProjectThumb project={project} />
+      <ProjectThumb project={p} />
       <div className="pl-4">
         <div className="mb-3 space-y-1">
           <Text as="p" variant="bodyMd" fontWeight="bold">
-            {project.name}
+            {p.name}
           </Text>
-          <Tag>{project.conformsTo?.name}</Tag>
+          <Tag>{p.conformsTo?.name}</Tag>
         </div>
         <div className="font-mono">
           <Text as="p" variant="bodyMd">
             <span className="font-bold">{"ID: "}</span>
-            <span>{project.id}</span>
+            <span>{p.id}</span>
           </Text>
           <Text as="p" variant="bodyMd">
             <span className="font-bold">{"Owner: "}</span>
-            <span>{project.primaryAccountable?.name}</span>
+            <span>{p.primaryAccountable?.name}</span>
           </Text>
         </div>
       </div>
     </div>
   );
 }
+
+export const SEARCH_PROJECT = gql`
+  query SearchProject($id: ID!) {
+    economicResource(id: $id) {
+      id
+      name
+      conformsTo {
+        name
+      }
+      primaryAccountable {
+        name
+      }
+    }
+  }
+`;
