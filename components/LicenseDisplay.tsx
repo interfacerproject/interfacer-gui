7f7066042e3b1f40de6178f3a06da3c69ffecd13
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-02-08 11:20:28 +0100

    fix(LicenseStep): 🐛 update form logic

--- components/LicenseDisplay.tsx
+++ components/LicenseDisplay.tsx
@@ -1,22 +1,32 @@
 import { Link, Stack, Text } from "@bbtgnn/polaris-interfacer";
 import { License } from "lib/licenses/types";
+import { getLicenseById } from "lib/licenses/utils";
 
 export interface Props {
-  label: string;
-  license: License;
+  label?: string;
+  license?: License;
+  licenseId?: string;
 }
 
 export default function LicenseDisplay(props: Props) {
-  const { label, license } = props;
+  const { label, license, licenseId } = props;
+
+  let l: License;
+
+  if (license) l = license;
+  else if (licenseId) l = getLicenseById(licenseId)!;
+  else return <></>;
 
   return (
     <Stack vertical spacing="extraTight">
-      <Text as="p" variant="bodyMd" fontWeight="bold">
-        {label}
-      </Text>
+      {label && (
+        <Text as="p" variant="bodyMd" fontWeight="bold">
+          {label}
+        </Text>
+      )}
       <Text as="p" variant="bodyMd">
-        <Link external url={license.reference}>
-          {license.name}
+        <Link external url={l.reference}>
+          {l.name}
         </Link>
       </Text>
     </Stack>
