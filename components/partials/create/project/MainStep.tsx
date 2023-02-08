e8540d2e9de740f48963ca0f96183ec12e02c4ea
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-02-08 14:07:49 +0100

    fix(MainStep): 🐛 fixed form logic

--- components/partials/create/project/MainStep.tsx
+++ components/partials/create/project/MainStep.tsx
@@ -2,6 +2,8 @@ import { Stack, TextField } from "@bbtgnn/polaris-interfacer";
 import BrMdEditor from "components/brickroom/BrMdEditor";
 import PTitleSubtitle from "components/polaris/PTitleSubtitle";
 import SelectTags from "components/SelectTags";
+import { SelectOption } from "components/types";
+import { isRequired } from "lib/isFieldRequired";
 import { url } from "lib/regex";
 import { useTranslation } from "next-i18next";
 import { Controller, useFormContext } from "react-hook-form";
@@ -14,7 +16,7 @@ export interface MainStepValues {
   title: string;
   description: string;
   link: string;
-  tags: Array<string>;
+  tags: Array<SelectOption>;
 }
 
 export const mainStepDefaultValues: MainStepValues = {
@@ -36,7 +38,7 @@ export const mainStepSchema = yup.object({
 export default function MainStep() {
   const { t } = useTranslation();
 
-  const { formState, control, setValue, watch } = useFormContext<CreateProjectValues>();
+  const { formState, control, setValue } = useFormContext<CreateProjectValues>();
   const { errors } = formState;
 
   //
@@ -44,8 +46,6 @@ export default function MainStep() {
   return (
     <Stack vertical spacing="extraLoose">
       <PTitleSubtitle title={t("General information")} />
-
-      <pre>{JSON.stringify(errors)}</pre>
 
       <Controller
         control={control}
@@ -61,8 +61,8 @@ export default function MainStep() {
             onBlur={onBlur}
             placeholder={t("My new project")}
             label={t("Title")}
-            // error={errors['main']['title'].message}
-            // requiredIndicator={isRequired(schema, name)}
+            error={errors.main?.title?.message}
+            requiredIndicator={isRequired(mainStepSchema, "title")}
           />
         )}
       />
@@ -81,8 +81,8 @@ export default function MainStep() {
             onBlur={onBlur}
             label={t("External link")}
             placeholder={t("projectsite.com/info")}
-            // error={errors[name]?.message}
-            // requiredIndicator={isRequired(schema, name)}
+            error={errors.main?.link?.message}
+            requiredIndicator={isRequired(mainStepSchema, "link")}
           />
         )}
       />
@@ -97,8 +97,8 @@ export default function MainStep() {
         onChange={({ text, html }) => {
           setValue("main.description", text);
         }}
-        // requiredIndicator={isRequired(schema, "description")}
-        // error={errors.description?.message}
+        requiredIndicator={isRequired(mainStepSchema, "description")}
+        error={errors.main?.description?.message}
       />
 
       <Controller
@@ -111,13 +111,13 @@ export default function MainStep() {
             ref={ref}
             onBlur={onBlur}
             onChange={onChange}
-            label={`${t("Tags")}:`}
+            label={t("Tags")}
             isMulti
             placeholder={t("Open-source, 3D Printing, Medical use")}
             helpText={t("Select a tag from the list, or type to create a new one")}
             creatable={true}
-            // error={errors[name]?.message}
-            // requiredIndicator={isRequired(schema, name)}
+            error={errors.main?.tags?.message}
+            requiredIndicator={isRequired(mainStepSchema, "tags")}
           />
         )}
       />
