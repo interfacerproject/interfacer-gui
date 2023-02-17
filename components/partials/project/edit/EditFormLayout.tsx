6ce5dbf673d7d63ef094368936c7118b2de3e21a
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-02-17 19:01:25 +0100

    feat(EditProject): ✨ created EditFormLayout

---
+++ components/partials/project/edit/EditFormLayout.tsx
@@ -0,0 +1,42 @@
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+// Components
+import LoadingOverlay from "components/LoadingOverlay";
+import { FieldValues, FormProvider, UseFormReturn } from "react-hook-form";
+import SubmitChangesBar from "./SubmitChangesBar";
+
+//
+
+export interface EditFormLayoutProps<T extends FieldValues> {
+  children: React.ReactNode;
+  formMethods: UseFormReturn<T, any>;
+  onSubmit: (values: T) => Promise<void>;
+}
+
+//
+
+export default function EditFormLayout<T extends FieldValues>(props: EditFormLayoutProps<T>) {
+  const router = useRouter();
+  const { children, formMethods, onSubmit = () => {} } = props;
+  const { handleSubmit } = formMethods;
+  const [loading, setLoading] = useState(false);
+
+  async function onSubmitWrapper(values: T) {
+    setLoading(true);
+    await onSubmit(values);
+    router.reload();
+  }
+
+  return (
+    <FormProvider {...formMethods}>
+      <form onSubmit={handleSubmit(onSubmitWrapper)} className="space-y-8">
+        <SubmitChangesBar />
+
+        <div className="mx-auto max-w-xl p-6 pb-24">{children}</div>
+      </form>
+
+      {loading && <LoadingOverlay />}
+    </FormProvider>
+  );
+}
