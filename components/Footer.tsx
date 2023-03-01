b1e7a1e9117aa4a036d5d31d25e03864b428d30a
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-03-01 12:08:45 +0100

    fix(footer): 🐛 changed spacing and sizes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,25 +1,45 @@
+import { Text } from "@bbtgnn/polaris-interfacer";
 import { useTranslation } from "next-i18next";
-import DyneLogo from "./DyneLogo";
 
 const Footer = () => {
   const { t } = useTranslation("common");
+
   return (
-    <>
-      <div className="container mx-auto p-4">
-        <div className="flex lg:items-center lg:justify-between flex-col lg:flex-row space-x-6 space-y-6">
-          <p>{t("Software by")}</p>
-          <DyneLogo />
-          <p className="max-w-xs">
-            {t("Project funded")}{" "}
-            <a className="text-bold font-semibold" href="https://interfacerproject.eu" target="_blank" rel="noreferrer">
-              {t("INTERFACER Project")}
-            </a>
-          </p>
-          <img src="/logo-eu-white.png" className="h-full max-w-fit" />
-          <img src="/logo-bwi-white.png" className="h-full max-w-fit" />
+    <div className="border-t-1 border-t-border-subdued">
+      <div className="container mx-auto p-6">
+        <div
+          className="
+        flex flex-col items-start space-y-6
+        md:flex-row md:items-center md:justify-center md:space-x-6 md:space-y-0
+      "
+        >
+          <div className="flex items-center space-x-2">
+            <Text as="p" variant="bodySm">
+              {t("Software by")}
+            </Text>
+            <img className="h-10" src="/logo-dyne.svg" alt="Dyne.org Logo" />
+          </div>
+
+          <div className="max-w-[250px] md:pl-4">
+            <Text as="p" variant="bodySm">
+              {t("Project funded")}{" "}
+              <a
+                className="font-bold underline hover:no-underline"
+                href="https://interfacerproject.eu"
+                target="_blank"
+                rel="noreferrer"
+              >
+                {t("INTERFACER Project")}
+              </a>
+            </Text>
+          </div>
+
+          <img className="h-10" src="/logo-eu.svg" alt="European Union Logo" />
+
+          <img className="h-10" src="/logo-bwi.svg" alt="Hamburg City Logo" />
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
