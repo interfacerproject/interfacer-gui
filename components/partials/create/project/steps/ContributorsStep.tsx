c4c6355738a4a6de49d04cbbb18cc9442243f16d
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-03-02 18:27:10 +0100

    fix(ContributorsStep): 🐛 hide self when searching users

--- components/partials/create/project/steps/ContributorsStep.tsx
+++ components/partials/create/project/steps/ContributorsStep.tsx
@@ -7,6 +7,7 @@ import BrUserDisplay from "components/brickroom/BrUserDisplay";
 import PCardWithAction from "components/polaris/PCardWithAction";
 import PTitleSubtitle from "components/polaris/PTitleSubtitle";
 import SearchUsers from "components/SearchUsers";
+import { useAuth } from "hooks/useAuth";
 import { formSetValueOptions } from "lib/formSetValueOptions";
 import { Person } from "lib/types";
 import { useFormContext } from "react-hook-form";
@@ -24,6 +25,7 @@ export const contributorsStepDefaultValues: ContributorsStepValues = [];
 export default function ContributorsStep() {
   const { t } = useTranslation();
   const { setValue, watch } = useFormContext<CreateProjectValues>();
+  const { user } = useAuth();
 
   const CONTRIBUTORS_FORM_KEY = "contributors";
   const contributors = watch(CONTRIBUTORS_FORM_KEY);
@@ -44,7 +46,7 @@ export default function ContributorsStep() {
   return (
     <Stack vertical spacing="extraLoose">
       <PTitleSubtitle title={t("Contributors")} subtitle={t("Tell us who contributed to this project.")} />
-      <SearchUsers onSelect={handleSelect} excludeIDs={contributors} />
+      <SearchUsers onSelect={handleSelect} excludeIDs={[...contributors, user?.ulid!]} />
       {contributors.length && (
         <Stack vertical spacing="tight">
           <Text variant="bodyMd" as="p">
