1f5c1d0760009165f246382695000d4dc3187c19
phoebus-84 <ennio@dyne.org>
2023-03-02 23:57:42 +0100

    refactor: ♻️ restyle search page

--- components/AgentsTable.tsx
+++ components/AgentsTable.tsx
@@ -58,9 +58,6 @@ export default function AgentsTable(props: AgentsTableProps) {
     dataQueryIdentifier,
   });
 
-  const [showFilter, setShowFilter] = useState(false);
-  const toggleFilter = () => setShowFilter(!showFilter);
-
   return (
     <>
       {loading && (
@@ -76,32 +73,12 @@ export default function AgentsTable(props: AgentsTableProps) {
           >
             {/* Left side */}
             {!hideHeader && <h3>{t("Projects")}</h3>}
-
-            {/* Right side */}
-            <button
-              onClick={toggleFilter}
-              className={cn(
-                "gap-2 text-white-700 font-normal normal-case rounded-[4px] border-1 btn btn-sm btn-outline border-white-600 bg-white-100 hover:text-accent hover:bg-white-100",
-                { "bg-accent text-white-100": showFilter },
-                { "float-right": hideHeader }
-              )}
-            >
-              <AdjustmentsIcon className="w-5 h-5" /> {t("Filter by")}
-            </button>
           </div>
           {/* Table and filters */}
           <div className="flex flex-row flex-nowrap items-start space-x-8">
             {data && (
               <div className="grow">
                 <AgentsTableBase data={data} onLoadMore={loadMore} hidePagination={hidePagination} />
-              </div>
-            )}
-            {showFilter && (
-              <div className="basis-96 sticky top-8">
-                <div className="p-4 bg-white border rounded-lg shadow space-y-6">
-                  <h4 className="text-xl font-bold capitalize">{t("filters")}:</h4>
-                  <div className="space-y-4">{searchFilter}</div>
-                </div>
               </div>
             )}
           </div>
--- pages/search.tsx
+++ pages/search.tsx
@@ -14,22 +14,22 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import { Checkbox, Select } from "@bbtgnn/polaris-interfacer";
+import { Checkbox, Stack, Tabs } from "@bbtgnn/polaris-interfacer";
+import { Cube, Events } from "@carbon/icons-react";
+import ProjectsCards from "components/ProjectsCards";
 import { useTranslation } from "next-i18next";
 import { useRouter } from "next/router";
 import { ReactElement, useState } from "react";
+import AgentsTable from "../components/AgentsTable";
 import Layout from "../components/layout/SearchLayout";
 import SearchBar from "../components/SearchBar";
-import { NextPageWithLayout } from "./_app";
-import ProjectsTable from "../components/ProjectsTable";
 import useFilters from "../hooks/useFilters";
-import AgentsTable from "../components/AgentsTable";
-import devLog from "../lib/devLog";
+import { NextPageWithLayout } from "./_app";
 
 const Search: NextPageWithLayout = () => {
   const { q } = useRouter().query;
   const [checked, setChecked] = useState(false);
-  const [searchType, setSearchType] = useState("projects");
+  const [searchType, setSearchType] = useState(0);
   const { proposalFilter } = useFilters();
   const { t } = useTranslation("common");
 
@@ -48,11 +48,6 @@ const Search: NextPageWithLayout = () => {
 
   //
 
-  const searchTypes = ["projects", "people"];
-  const onChangeSearch = (value: string) => {
-    setSearchType(value);
-  };
-
   return (
     <div className="container mx-auto">
       <div className="p-8">
@@ -61,41 +56,44 @@ const Search: NextPageWithLayout = () => {
             <span className="text-[#5DA091]">{t("Search result for") + ": "}</span>
             {`"${q}"`}
           </h1>
+
           <div className="flex flex-col gap-2 my-8">
             <SearchBar />
             <Checkbox label={t("do not search inside the the description")} checked={checked} onChange={setChecked} />
           </div>
         </div>
-        {searchType === "projects" && (
-          <ProjectsTable
-            filter={filters}
-            hideHeader
-            searchFilter={
-              <Select
-                options={searchTypes}
-                value={searchType}
-                onChange={onChangeSearch}
-                label={t("Select search type")}
-                placeholder={t("Select search type")}
-              />
-            }
+        <Stack vertical spacing="loose">
+          <Tabs
+            tabs={[
+              {
+                id: "projects",
+                content: (
+                  <span className="flex items-center gap-2">
+                    <Cube />
+                    {t("Projects")}
+                  </span>
+                ),
+                accessibilityLabel: t("Project overview"),
+                panelID: "overview-content",
+              },
+              {
+                id: "people",
+                content: (
+                  <span className="flex items-center gap-2">
+                    <Events />
+                    {t("People")}
+                  </span>
+                ),
+                accessibilityLabel: t("Relationship tree"),
+                panelID: "relationships-content",
+              },
+            ]}
+            selected={searchType}
+            onSelect={setSearchType}
           />
-        )}
-        {searchType === "people" && (
-          <AgentsTable
-            hideHeader
-            searchTerm={q?.toString() || ""}
-            searchFilter={
-              <Select
-                options={searchTypes}
-                value={searchType}
-                onChange={onChangeSearch}
-                label={t("Select search type")}
-                placeholder={t("Select search type")}
-              />
-            }
-          />
-        )}
+          {searchType === 0 && <ProjectsCards filter={filters} />}
+          {searchType === 1 && <AgentsTable hideHeader searchTerm={q?.toString() || ""} />}
+        </Stack>
         {/*<ProjectsCards filter={Projectfilter} />*/}
         {/*<ResourcesCards filter={Resourcefilter} />*/}
       </div>
