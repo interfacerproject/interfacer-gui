044ae89259af7a58652710529200a6f565f41ba9
phoebus-84 <ennio@dyne.org>
2023-03-20 18:35:59 +0100

    feat: ✨ add iubenda privacy policy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -41,12 +41,22 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   // Use the layout defined at the page level or default layout
   const getLayout = Component.getLayout || ((page: ReactElement) => <Layout>{page}</Layout>);
   const publicPage = Component.publicPage || false;
+  const iubenda = `(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);`;
+
   return (
     <AppProvider i18n={enTranslations}>
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <script type="text/javascript" dangerouslySetInnerHTML={{ __html: iubenda }} />
       </Head>
       <AuthProvider publicPage={publicPage}>{getLayout(<Component {...pageProps} />)}</AuthProvider>
+      <a
+        href="https://www.iubenda.com/privacy-policy/75616407"
+        className="iubenda-white iubenda-noiframe iubenda-embed iubenda-noiframe "
+        title="Privacy Policy "
+      >
+        {"Privacy Policy"}
+      </a>
     </AppProvider>
   );
 }
