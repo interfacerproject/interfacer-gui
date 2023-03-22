632d7b0e52ecb9b8c0f9ad67a0ec7accd8f24d90
phoebus-84 <83974413+phoebus-84@users.noreply.github.com>
2023-03-22 16:06:15 +0100

    fix: 🐛 show points and trends for cycle (#552)
    Co-authored-by: phoebus-84 <ennio@dyne.org>

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -24,7 +24,7 @@ import ProjectsCards from "components/ProjectsCards";
 import TokensResume from "components/TokensResume";
 import { useAuth } from "hooks/useAuth";
 import useStorage from "hooks/useStorage";
-import { Token } from "hooks/useWallet";
+import { Token, TrendPeriod } from "hooks/useWallet";
 import { CLAIM_DID, FETCH_USER } from "lib/QueryAndMutation";
 import type { GetStaticPaths, NextPage } from "next";
 import { useTranslation } from "next-i18next";
@@ -85,16 +85,16 @@ const Profile: NextPage = () => {
 
   const tabsContent = [
     <div className="flex" key={"week"}>
-      <TokensResume stat={t(Token.Idea)} id={idToBeFetch!} />
-      <TokensResume stat={t(Token.Strengths)} id={idToBeFetch!} />
+      <TokensResume stat={t(Token.Idea)} id={idToBeFetch!} period={TrendPeriod.Week} />
+      <TokensResume stat={t(Token.Strengths)} id={idToBeFetch!} period={TrendPeriod.Week} />
     </div>,
     <div className="flex" key={"month"}>
-      <TokensResume stat={t(Token.Idea)} id={idToBeFetch!} />
-      <TokensResume stat={t(Token.Strengths)} id={idToBeFetch!} />
+      <TokensResume stat={t(Token.Idea)} id={idToBeFetch!} period={TrendPeriod.Month} />
+      <TokensResume stat={t(Token.Strengths)} id={idToBeFetch!} period={TrendPeriod.Month} />
     </div>,
     <div className="flex" key="cyclal">
-      <TokensResume stat={t(Token.Idea)} id={idToBeFetch!} />
-      <TokensResume stat={t(Token.Strengths)} id={idToBeFetch!} />
+      <TokensResume stat={t(Token.Idea)} id={idToBeFetch!} period={TrendPeriod.Cycle} />
+      <TokensResume stat={t(Token.Strengths)} id={idToBeFetch!} period={TrendPeriod.Cycle} />
     </div>,
   ];
 
