11215bb2b4d48c734b338ff2f1806d1becf96e7a
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-03-23 20:31:55 +0100

    feat: ✨ created ProjectTypeRenderProps

--- components/ProjectTypeChip.tsx
+++ components/ProjectTypeChip.tsx
@@ -1,10 +1,10 @@
 import { Collaborate, DataDefinition, GroupObjectsNew } from "@carbon/icons-react";
 import classNames from "classnames";
-import { isProjectType } from "lib/isProjectType";
 import { EconomicResource } from "lib/types";
 import { useTranslation } from "next-i18next";
 import { ReactNode } from "react";
 import LinkWrapper from "./LinkWrapper";
+import { ProjectTypeRenderProps } from "./ProjectTypeRenderProps";
 import { ProjectType } from "./types";
 
 //
@@ -28,22 +28,21 @@ export default function ProjectTypeChip(props: Props) {
   const name = project?.conformsTo?.name as ProjectType;
   const href = `/projects?conformsTo=${project?.conformsTo?.id}`;
 
-  const isType = isProjectType(name);
+  const renderProps = ProjectTypeRenderProps[name];
 
-  const classes = classNames("flex items-center space-x-1 py-1 px-2 rounded w-fit", {
-    "hover:ring-2 hover:cursor-pointer": link,
-    "bg-[#E4CCE3] text-[#413840]": isType.Design,
-    "hover:ring-[#C18ABF]": isType.Design && link,
-    "bg-[#FAE5B7] text-[#614C1F]": isType.Product,
-    "hover:ring-[#614C1F]": isType.Product && link,
-    "bg-[#CDE0E4] text-[#024960] ": isType.Service,
-    "hover:ring-[#5D8CA0]": isType.Service && link,
-  });
+  const hoverClasses = `hover:cursor-pointer hover:ring-2 hover:${renderProps.classes.border}`;
+  const classes = classNames(
+    "flex items-center space-x-1 py-1 px-2 rounded w-fit",
+    `${renderProps.classes.bg} ${renderProps.classes.content}`,
+    {
+      [hoverClasses]: link,
+    }
+  );
 
   const baseChip = (
     <span className={classes}>
-      <strong>{name}</strong>
-      {icons[name]}
+      <strong>{renderProps.label}</strong>
+      <renderProps.icon size={16} />
     </span>
   );
 
