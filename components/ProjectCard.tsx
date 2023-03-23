67e756410e2ba2ed624ee91a58eac6f0237c0b45
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-03-23 17:50:07 +0100

    feat: ✨ added border on hover

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,7 +1,9 @@
 import { Text } from "@bbtgnn/polaris-interfacer";
+import classNames from "classnames";
 import { useAuth } from "hooks/useAuth";
 import { Agent, EconomicResource } from "lib/types";
 import Link from "next/link";
+import { useState } from "react";
 import AddStar from "./AddStar";
 import BrTags from "./brickroom/BrTags";
 import BrUserAvatar from "./brickroom/BrUserAvatar";
@@ -17,17 +19,23 @@ export default function ProjectCard(props: ProjectCardProps) {
   const { project } = props;
   const { user } = useAuth();
 
-  console.log("project", project.primaryAccountable);
+  const [hover, setHover] = useState(false);
+  const setHoverTrue = () => setHover(true);
+  const setHoverFalse = () => setHover(false);
+
+  const classes = classNames("rounded-lg bg-white shadow", {
+    "ring-2 ring-primary": hover,
+  });
 
   return (
-    <div className="rounded-lg bg-white shadow ">
+    <div className={classes}>
       <div className="space-y-3 p-3">
         <div className="flex justify-between items-center">
           <UserDisplay user={project.primaryAccountable!} />
           {user && <AddStar id={project.id!} owner={project.primaryAccountable!.id} tiny />}
         </div>
 
-        <div className="">
+        <div onMouseOver={setHoverTrue} onMouseLeave={setHoverFalse}>
           <Link href={`/project/${project.id}`}>
             <a className="space-y-3">
               <ProjectImage
@@ -38,9 +46,7 @@ export default function ProjectCard(props: ProjectCardProps) {
               <Text variant="headingLg" as="h4">
                 {project.name}
               </Text>
-              <div className="flex justify-between">
-                <ProjectTypeChip projectNode={project} noIntroduction />
-              </div>
+              <ProjectTypeChip projectNode={project} noIntroduction />
             </a>
           </Link>
         </div>
