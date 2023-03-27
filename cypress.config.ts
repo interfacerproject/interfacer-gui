20168f946e19cd2d6813a304bf56d05d57ffa351
phoebus-84 <ennio@dyne.org>
2023-03-27 12:26:02 +0200

    test: 🚨 add ecperimenatl memory managment

--- cypress.config.ts
+++ cypress.config.ts
@@ -22,6 +22,7 @@ export default defineConfig({
     baseUrl: "http://localhost:3000",
     experimentalStudio: true,
     testIsolation: false,
+    experimentalMemoryManagement: true,
     setupNodeEvents(on, config) {
       require("cypress-localstorage-commands/plugin")(on, config);
       require("cypress-fail-fast/plugin")(on, config);
