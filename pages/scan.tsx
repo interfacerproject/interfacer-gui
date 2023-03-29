b5960db3e47de53221b3545454aaa77669c0cf3c
phoebus-84 <ennio@dyne.org>
2023-03-29 19:38:52 +0200

    refactor: ♻️ use stack in scan page

--- pages/scan.tsx
+++ pages/scan.tsx
@@ -1,3 +1,4 @@
+import { Stack } from "@bbtgnn/polaris-interfacer";
 import DppReader from "components/DppReader";
 import PTitleSubtitle from "components/polaris/PTitleSubtitle";
 import { useTranslation } from "next-i18next";
@@ -5,14 +6,16 @@ import { useTranslation } from "next-i18next";
 const Scan = () => {
   const { t } = useTranslation("common");
   return (
-    <div className="container mx-auto pt-10">
-      <PTitleSubtitle
-        title={t("Scan your QR code")}
-        subtitle={t(
-          "Discover contributors, related projects, Digital Product Passport of your resource using your phone"
-        )}
-      />
-      <DppReader />
+    <div className="container mx-auto">
+      <Stack vertical>
+        <PTitleSubtitle
+          title={t("Scan your QR code")}
+          subtitle={t(
+            "Discover contributors, related projects, Digital Product Passport of your resource using your phone"
+          )}
+        />
+        <DppReader />
+      </Stack>
     </div>
   );
 };
