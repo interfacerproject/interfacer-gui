118ce6313ff4d261b271afb0c8cbe0c9619f1d35
Puria Nafisi Azizi <puria@dyne.org>
2023-03-31 05:16:00 +0200

    fix: 🐛 error proof and loosy data render on missing attrs

--- components/ProjectTypeRoundIcon.tsx
+++ components/ProjectTypeRoundIcon.tsx
@@ -2,13 +2,13 @@ import { ProjectTypeRenderProps } from "./ProjectTypeRenderProps";
 import { ProjectType } from "./types";
 
 export default function ProjectTypeRoundIcon(props: { projectType?: ProjectType }) {
-  const { projectType = ProjectType.DESIGN } = props;
-  const renderProps = ProjectTypeRenderProps[projectType];
+  const { projectType } = props;
+  const renderProps = ProjectTypeRenderProps[projectType || ProjectType.DESIGN];
 
   return (
-    <div className={`w-fit h-fit p-5 rounded-full ${renderProps.classes.bg} ${renderProps.classes.content}`}>
+    <div className={`w-fit h-fit p-5 rounded-full ${renderProps?.classes.bg} ${renderProps?.classes.content}`}>
       {/* @ts-ignore */}
-      {<renderProps.icon size={40} />}
+      {renderProps && <renderProps.icon size={40} />}
     </div>
   );
 }
