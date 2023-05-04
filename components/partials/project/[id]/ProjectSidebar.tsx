5bc1f7a5d4498601ddc3d50ec0042a364e38cee3
phoebus-84 <83974413+phoebus-84@users.noreply.github.com>
2023-05-04 10:30:56 +0200

    feat: ✨ add share button for projects (#661)
    Co-authored-by: phoebus-84 <ennio@dyne.org>
    Co-authored-by: Puria Nafisi Azizi <puria@dyne.org>

--- components/partials/project/[id]/ProjectSidebar.tsx
+++ components/partials/project/[id]/ProjectSidebar.tsx
@@ -1,16 +1,23 @@
+import { Button, Icon, Popover, Stack } from "@bbtgnn/polaris-interfacer";
+import { ShareMinor } from "@shopify/polaris-icons";
 import AddStar from "components/AddStar";
-import { useProject } from "components/layout/FetchProjectLayout";
 import OshTool from "components/Osh";
-import TechnicalInfoCard from "./sidebar/TechnicalInfoCard";
+
+import { useProject } from "components/layout/FetchProjectLayout";
+import { useCallback, useState } from "react";
+
 import ContributionsCard from "./sidebar/ContributionsCard";
 import RelationsCard from "./sidebar/RelationsCard";
 import SocialCard from "./sidebar/SocialCard";
+import TechnicalInfoCard from "./sidebar/TechnicalInfoCard";
+import ShareButton from "components/ShareButton";
 
 export default function ProjectSidebar() {
   const { project } = useProject();
   return (
     <div className="lg:col-span-1 order-first lg:order-last">
-      <div className="w-full justify-end flex pb-3">
+      <div className="w-full justify-end flex pb-3 gap-2">
+        <ShareButton />
         <AddStar id={project.id!} owner={project.primaryAccountable!.id} />
       </div>
       <SocialCard />
