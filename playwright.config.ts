beffe320a1fd9bc9ed87ebde42407020af84c136
Puria Nafisi Azizi <puria@dyne.org>
2023-05-10 16:12:21 +0200

    chore: 🔧 fix CI for playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,10 +3,15 @@ import dotenv from "dotenv";
 
 dotenv.config({ path: "./playwright.env" });
 
+const PORT = process.env.PORT || 3000;
+const TEST_BASE_URL = process.env.TEST_BASE_URL || `http://localhost:${PORT}`;
+
 export default defineConfig({
   webServer: {
     command: "pnpm build && pnpm start",
-    url: "http://localhost:3000",
+    url: TEST_BASE_URL,
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
   },
   testDir: "./tests",
   /* Run tests in files in parallel */
@@ -22,7 +27,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://localhost:3000",
+    baseURL: TEST_BASE_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -38,11 +43,6 @@ export default defineConfig({
     {
       name: "firefox",
       use: { ...devices["Desktop Firefox"] },
-    },
-
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
     },
 
     /* Test against mobile viewports. */
@@ -65,11 +65,4 @@ export default defineConfig({
     //   use: { ..devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
