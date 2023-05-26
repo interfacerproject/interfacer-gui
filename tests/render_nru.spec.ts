a401c162868bab9ca8a89ada8161b4f1fbada06e
phoebus-84 <83974413+phoebus-84@users.noreply.github.com>
2023-05-26 16:22:37 +0200

    test: 🚨 add tests for page as not registered users (#730)
    Co-authored-by: phoebus-84 <ennio@dyne.org>

--- tests/render_nru.spec.ts
+++ tests/render_nru.spec.ts
@@ -1,10 +1,7 @@
-import { Page, expect, test } from "@playwright/test";
+import { expect } from "@playwright/test";
+import { EnvVariables, test } from "./fixtures/test";
 
 test.describe("When user is not logged in", () => {
-  let page: Page;
-  test.beforeEach(async ({ context }) => {
-    page = await context.newPage();
-  });
   test("Should see /", async ({ page }) => {
     await page.goto("");
     await expect(page.getByRole("heading", { name: "Empowering the Open Source Hardware Community" })).toBeVisible();
@@ -20,5 +17,20 @@ test.describe("When user is not logged in", () => {
   test("Should see /sign_up", async ({ page }) => {
     await page.goto("/sign_up");
     await expect(page.getByText("Sign up")).toBeVisible();
+  });
+
+  test("Should see /project/:id", async ({ page, envVariables }) => {
+    await page.goto(`/project/${envVariables.PROJECT_ID}`);
+    await expect(page.getByText("Project")).toBeTruthy();
+  });
+
+  test("Should see /resource/:id", async ({ page, envVariables }) => {
+    await page.goto(`/resource/${envVariables.RESOURCE_ID}`);
+    await expect(page.getByText(envVariables.RESOURCE_ID)).toBeVisible();
+  });
+
+  test("Should see /resources", async ({ page }) => {
+    await page.goto("/resources");
+    await expect(page.getByText("Resources")).toBeTruthy();
   });
 });
