bb392a7df75e584b4040a03540441585aa88506d
phoebus-84 <ennio@dyne.org>
2023-05-29 10:41:56 +0200

    test: 🚨 dont test invitation key

--- tests/authentication.spec.ts
+++ tests/authentication.spec.ts
@@ -43,8 +43,8 @@ test.describe("Authentication process", () => {
     await page.goto("/sign_up");
 
     //Enter invitation key
-    await page.fill("#invitationKey", authVariables.invitationKey!);
-    await page.getByRole("button", { name: "Next step" }).click();
+    // await page.fill("#invitationKey", authVariables.invitationKey!);
+    // await page.getByRole("button", { name: "Next step" }).click();
 
     // Enter email and submit
     await page.fill("#email", random.randomEmail());
