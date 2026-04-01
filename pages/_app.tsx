f574497618833643d80669aeb46ada53e5dcd8f8
phoebus-84 <ennio@dyne.org>
2026-04-01 11:21:36 +0200

    fix(fonts): 🐛 resolve @fontsource weight imports for Next.js

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,8 +14,13 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import "@fontsource/ibm-plex-sans";
-import "@fontsource/space-grotesk";
+import "@fontsource/ibm-plex-sans/400";
+import "@fontsource/ibm-plex-sans/500";
+import "@fontsource/ibm-plex-sans/600";
+import "@fontsource/ibm-plex-sans/700";
+import "@fontsource/space-grotesk/400";
+import "@fontsource/space-grotesk/500";
+import "@fontsource/space-grotesk/700";
 
 import Layout from "components/layout/Layout";
 import { AuthProvider } from "contexts/AuthContext";
